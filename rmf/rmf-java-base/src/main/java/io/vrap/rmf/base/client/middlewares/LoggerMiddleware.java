74cd89fe4fa63a1df6f5eed614c96c6749704aa3
alex-ct <aleksandar.petrovic@commercetools.de>
2019-09-19 15:35:03 +0200

    added simple temp logger

--- rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/middlewares/LoggerMiddleware.java
+++ rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/middlewares/LoggerMiddleware.java
@@ -2,14 +2,10 @@ package io.vrap.rmf.base.client.middlewares;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.vrap.rmf.base.client.utils.json.VrapJsonUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.CompletableFuture;
 
 public final class LoggerMiddleware implements Middleware {
-
-    private static final Logger LOGGER = LogManager.getLogger("VRAP_JSON_LOGGER");
     
     @Override
     public CompletableFuture<MiddlewareArg> next(MiddlewareArg arg) {
@@ -23,7 +19,7 @@ public final class LoggerMiddleware implements Middleware {
             loggerMessage.setResponseBody(new String(arg.getResponse().getBody()));
             
             String loggerMessageString = VrapJsonUtils.toJsonString(loggerMessage);
-            LOGGER.info(loggerMessageString);
+            System.out.println(loggerMessageString);
         }catch (JsonProcessingException error){
             MiddlewareArg args = MiddlewareArg.from(arg.getRequest(), arg.getResponse(), error, arg.getNext());
             return arg.getNext().next(args);

4c2929d52b9d0f646a8d881c5308e9545a6f422e
alex-ct <aleksandar.petrovic@commercetools.de>
2019-10-17 10:26:31 +0200

    Minor build fix.

--- commercetools/commercetools-api-default/src/test/java/commercetools/misc/UserAgentHeaderTest.java
+++ commercetools/commercetools-api-default/src/test/java/commercetools/misc/UserAgentHeaderTest.java
@@ -1,16 +1,13 @@
 package commercetools.misc;
 
-import com.commercetools.client.ApiRoot;
-import com.commercetools.client.ByProjectKeyCategoriesGet;
+
+import com.commercetools.api.generated.client.ByProjectKeyCategoriesGet;
 import commercetools.utils.CommercetoolsTestUtils;
-import io.vrap.rmf.base.client.ApiHttpHeaders;
 import io.vrap.rmf.base.client.VrapHttpClient;
 import io.vrap.rmf.base.client.middlewares.HttpMiddleware;
-import io.vrap.rmf.base.client.middlewares.LoggerMiddleware;
 import io.vrap.rmf.base.client.middlewares.MiddlewareArg;
 import io.vrap.rmf.base.client.oauth2.ClientCredentialsTokenSupplier;
 import io.vrap.rmf.impl.okhttp.VrapOkhttpClient;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static commercetools.utils.CommercetoolsTestUtils.*;

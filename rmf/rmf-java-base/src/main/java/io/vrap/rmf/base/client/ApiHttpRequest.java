a12c56dc91cf6514e96f728ff0b864e8aa59cd30
Jens Schulze <jenschude@gmail.com>
2020-09-02 12:05:38 +0200

    add URI property to ApiHttpRequest

--- rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/ApiHttpRequest.java
+++ rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/ApiHttpRequest.java
@@ -1,27 +1,46 @@
 package io.vrap.rmf.base.client;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Map;
+import java.net.URI;
 
 public class ApiHttpRequest {
 
     private ApiHttpMethod method;
+    private URI uri;
+//    @Deprecated
     private String relativeUrl;
+//    @Deprecated
     private String baseUrl;
     private ApiHttpHeaders headers;
     private byte[] body;
+
 
     public ApiHttpRequest() {
 
     }
 
+//    @Deprecated
     public ApiHttpRequest(ApiHttpMethod method, String relativeUrl, String baseUrl, ApiHttpHeaders headers, byte[] body) {
         this.method = method;
         this.relativeUrl = relativeUrl;
         this.baseUrl = baseUrl;
         this.headers = headers;
         this.body = body;
+    }
+
+    public ApiHttpRequest(ApiHttpMethod method, URI uri, ApiHttpHeaders headers, byte[] body) {
+        this.method = method;
+        this.uri = uri;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public ApiHttpRequest(ApiHttpRequest r) {
+        this.method = r.method;
+        this.uri = r.uri;
+        this.headers = r.headers;
+        this.body = r.body;
+        this.relativeUrl = r.relativeUrl;
+        this.baseUrl = r.baseUrl;
     }
 
     public ApiHttpMethod getMethod() {
@@ -63,18 +82,30 @@ public class ApiHttpRequest {
         setBody(body.getBytes());
     }
 
+//    @Deprecated
     public String getRelativeUrl() {
         return relativeUrl;
     }
 
+//    @Deprecated
     public void setRelativeUrl(String relativeUrl) {
         this.relativeUrl = relativeUrl;
     }
 
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+//    @Deprecated
     public String getBaseUrl() {
         return baseUrl;
     }
 
+//    @Deprecated
     public void setBaseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
     }
@@ -87,5 +118,12 @@ public class ApiHttpRequest {
             return getBaseUrl();
         }
         return getBaseUrl() + getRelativeUrl();
+    }
+
+    public ApiHttpRequest resolve(URI baseUri)
+    {
+        ApiHttpRequest request = new ApiHttpRequest(this);
+        request.setUri(baseUri.resolve(this.uri));
+        return request;
     }
 }

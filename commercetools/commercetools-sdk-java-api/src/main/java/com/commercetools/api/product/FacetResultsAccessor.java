267fe14fe818c976e24dbab7642a969a8b3f53a7
Jens Schulze <jenschude@gmail.com>
2020-10-23 13:31:50 +0200

    refactor facet result accessor to provide static method

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/product/FacetResultsAccessor.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/product/FacetResultsAccessor.java
@@ -5,7 +5,9 @@ import com.commercetools.api.models.product.FacetResults;
 import io.vrap.rmf.base.client.Accessor;
 import io.vrap.rmf.base.client.utils.json.VrapJsonUtils;
 
+import javax.annotation.Nullable;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class FacetResultsAccessor extends Accessor<FacetResults> {
@@ -13,11 +15,18 @@ public class FacetResultsAccessor extends Accessor<FacetResults> {
         super(value);
     }
 
+    @Nullable
     public Map<String, FacetResult> facets()
     {
-        return value.values().entrySet().stream().collect(Collectors.toMap(
+        return asFacetResultMap(value);
+    }
+
+    @Nullable
+    public static Map<String, FacetResult> asFacetResultMap(FacetResults value)
+    {
+        return Optional.ofNullable(value.values()).map(values -> values.entrySet().stream().collect(Collectors.toMap(
                 Map.Entry::getKey,
                 entry -> VrapJsonUtils.getConfiguredObjectMapper().convertValue(entry.getValue(), FacetResult.class)
-        ));
+        ))).orElse(null);
     }
 }

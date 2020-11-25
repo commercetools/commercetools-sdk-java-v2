29df56e63a1ec21b1a22c69a25af86f4782563df
Jens Schulze <jenschude@gmail.com>
2020-11-25 14:27:48 +0100

    refactor generic Resource for models

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/DomainResource.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/DomainResource.java
@@ -1,13 +1,7 @@
 package com.commercetools.api.models;
 
-import java.time.ZonedDateTime;
-
 public interface DomainResource<T> {
     public String getId();
 
     public Long getVersion();
-
-    public ZonedDateTime getCreatedAt();
-
-    public ZonedDateTime getLastModifiedAt();
 }

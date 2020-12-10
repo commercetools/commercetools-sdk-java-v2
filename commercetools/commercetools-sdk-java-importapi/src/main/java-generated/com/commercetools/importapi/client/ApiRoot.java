12cc0d2853b719dc3928c068679e229f406f5e87
Jens Schulze <jenschude@gmail.com>
2020-12-10 14:44:25 +0100

    add Closeable interface to ApiRoot

--- commercetools/commercetools-sdk-java-importapi/src/main/java-generated/com/commercetools/importapi/client/ApiRoot.java
+++ commercetools/commercetools-sdk-java-importapi/src/main/java-generated/com/commercetools/importapi/client/ApiRoot.java
@@ -2,6 +2,8 @@ package com.commercetools.importapi.client;
 
 import io.vrap.rmf.base.client.ApiHttpClient;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.List;
 import java.util.Arrays;
 import io.vrap.rmf.base.client.utils.Generated;
@@ -10,7 +12,7 @@ import io.vrap.rmf.base.client.utils.Generated;
     value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator",
     comments = "https://github.com/vrapio/rmf-codegen"
 )
-public class ApiRoot {
+public class ApiRoot implements Closeable {
 
     private final ApiHttpClient apiHttpClient;
 
@@ -26,4 +28,8 @@ public class ApiRoot {
         return new ByProjectKeyRequestBuilder(this.apiHttpClient, projectKey);
     }
 
+    @Override
+    public void close() throws IOException {
+        apiHttpClient.close();
+    }
 }

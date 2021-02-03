dfeaa49ae98d614947280a5b8f71a780547567d5
Auto Mation <automation@commercetools.com>
2021-02-03 07:46:12 +0000

    TASK: Updating SDK

--- commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/client/ByProjectKeyCustomObjectsByContainerGet.java
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/client/ByProjectKeyCustomObjectsByContainerGet.java
@@ -13,12 +13,13 @@ import io.vrap.rmf.base.client.*;
 import io.vrap.rmf.base.client.utils.Generated;
 
 /**
-*  <p>Get CustomObject by container</p>
+*  <p>Get CustomObjectPagedQueryResponse by container</p>
 */
 @Generated(value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator", comments = "https://github.com/vrapio/rmf-codegen")
 public class ByProjectKeyCustomObjectsByContainerGet extends
-        ApiMethod<ByProjectKeyCustomObjectsByContainerGet, com.commercetools.api.models.custom_object.CustomObject>
-        implements com.commercetools.api.client.ExpandableTrait<ByProjectKeyCustomObjectsByContainerGet>,
+        ApiMethod<ByProjectKeyCustomObjectsByContainerGet, com.commercetools.api.models.custom_object.CustomObjectPagedQueryResponse>
+        implements com.commercetools.api.client.QueryTrait<ByProjectKeyCustomObjectsByContainerGet>,
+        com.commercetools.api.client.ExpandableTrait<ByProjectKeyCustomObjectsByContainerGet>,
         com.commercetools.api.client.ErrorableTrait<ByProjectKeyCustomObjectsByContainerGet>,
         com.commercetools.api.client.DeprecatableTrait<ByProjectKeyCustomObjectsByContainerGet> {
 
@@ -50,14 +51,15 @@ public class ByProjectKeyCustomObjectsByContainerGet extends
     }
 
     @Override
-    public ApiHttpResponse<com.commercetools.api.models.custom_object.CustomObject> executeBlocking(Duration timeout) {
+    public ApiHttpResponse<com.commercetools.api.models.custom_object.CustomObjectPagedQueryResponse> executeBlocking(
+            Duration timeout) {
         return blockingWait(execute(), timeout);
     }
 
     @Override
-    public CompletableFuture<ApiHttpResponse<com.commercetools.api.models.custom_object.CustomObject>> execute() {
+    public CompletableFuture<ApiHttpResponse<com.commercetools.api.models.custom_object.CustomObjectPagedQueryResponse>> execute() {
         return apiHttpClient().execute(this.createHttpRequest(),
-            com.commercetools.api.models.custom_object.CustomObject.class);
+            com.commercetools.api.models.custom_object.CustomObjectPagedQueryResponse.class);
     }
 
     public String getProjectKey() {
@@ -66,6 +68,10 @@ public class ByProjectKeyCustomObjectsByContainerGet extends
 
     public String getContainer() {
         return this.container;
+    }
+
+    public List<String> getWhere() {
+        return this.getQueryParam("where");
     }
 
     public List<String> getExpand() {
@@ -80,12 +86,28 @@ public class ByProjectKeyCustomObjectsByContainerGet extends
         this.container = container;
     }
 
+    public ByProjectKeyCustomObjectsByContainerGet withWhere(final String where) {
+        return copy().withQueryParam("where", where);
+    }
+
+    public ByProjectKeyCustomObjectsByContainerGet addWhere(final String where) {
+        return copy().addQueryParam("where", where);
+    }
+
     public ByProjectKeyCustomObjectsByContainerGet withExpand(final String expand) {
         return copy().withQueryParam("expand", expand);
     }
 
     public ByProjectKeyCustomObjectsByContainerGet addExpand(final String expand) {
         return copy().addQueryParam("expand", expand);
+    }
+
+    public ByProjectKeyCustomObjectsByContainerGet withPredicateVar(final String varName, final String predicateVar) {
+        return copy().withQueryParam(String.format("var.%s", varName), predicateVar);
+    }
+
+    public ByProjectKeyCustomObjectsByContainerGet addPredicateVar(final String varName, final String predicateVar) {
+        return copy().addQueryParam(String.format("var.%s", varName), predicateVar);
     }
 
     @Override

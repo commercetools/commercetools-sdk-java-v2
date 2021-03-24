23bfda775686354c44ef29c68f0b31d9225f989e
Barbara Palumbo <barbara.palumbo@commercetools.de>
2021-03-24 12:58:45 +0100

    clean a bit and add parameters to the array of object

--- commercetools/commercetools-sdk-java-api/src/test/java/ResourceByProjectKeyCategoriesTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/ResourceByProjectKeyCategoriesTest.java
@@ -25,7 +25,7 @@ import java.net.URI;
 public class ResourceByProjectKeyCategoriesTest {
 
     private final ApiHttpClient apiHttpClientMock = Mockito.mock(ApiHttpClient.class);
-    private final String projectKey = "test-projectkey";
+    private final String projectKey = "test_projectKey";
     private final ApiRoot apiRoot = createClient();
 
     public  final ApiRoot createClient() {
@@ -37,82 +37,55 @@ public class ResourceByProjectKeyCategoriesTest {
 
     @Test
     @Parameters(method = "requestParameters")
-    public void createHttpRequest(ApiHttpRequest apiHttpRequest) {
-
-
-
-        ByProjectKeyCategoriesGet byProjectKeyCategoriesGet = new ByProjectKeyCategoriesGet(apiHttpClientMock, projectKey);
-
-        Assert.assertEquals(apiHttpRequest.getMethod().toString(), byProjectKeyCategoriesGet.createHttpRequest().getMethod().toString());
+    public void createHttpRequest(ApiHttpRequest request, String httpMethod, String uri) {
+        Assert.assertEquals(httpMethod, request.getMethod().toString());
+        Assert.assertEquals(uri, request.getUri().toString());
     }
 
     private Object[] requestParameters() {
         return new Object[] {
-                new Object[] { new ApiHttpRequest(ApiHttpMethod.GET, URI.create("/test_projectKey/categories?expand=expand"), new ApiHttpHeaders(), null) },
-                new Object[] { new ApiHttpRequest(ApiHttpMethod.GET, URI.create("/test_projectKey/categories?sort=sort"), new ApiHttpHeaders(), null) },
-                new Object[] { new ApiHttpRequest(ApiHttpMethod.GET, URI.create("/test_projectKey/categories?limit=10"), new ApiHttpHeaders(), null) }
+                new Object[] { new ApiHttpRequest(ApiHttpMethod.GET, URI.create("/test_projectKey/categories?expand=expand"), new ApiHttpHeaders(), null), "GET", "/test_projectKey/categories?expand=expand" },
+                new Object[] { new ApiHttpRequest(ApiHttpMethod.GET, URI.create("/test_projectKey/categories?sort=sort"), new ApiHttpHeaders(), null), "GET", "/test_projectKey/categories?sort=sort" },
+                new Object[] { new ApiHttpRequest(ApiHttpMethod.GET, URI.create("/test_projectKey/categories?limit=10"), new ApiHttpHeaders(), null), "GET", "/test_projectKey/categories?limit=10" }
         };
     }
 
     @Test
-    @Parameters(method = "requestWithParameters")
-    public void withMethods(ApiHttpRequest request) {
-//        ApiHttpRequest request = apiRoot.withProjectKey("test_projectKey").categories().get().withExpand("expand").createHttpRequest();
+    @Parameters(method = "requestWithMethodParameters")
+    public void withMethods(ApiHttpRequest request, String httpMethod, String uri) {
 
-        Assert.assertEquals("GET", request.getMethod().toString());
-        Assert.assertEquals("/test_projectKey/categories?expand=expand", request.getUri().toString());
+        Assert.assertEquals(httpMethod, request.getMethod().toString());
+        Assert.assertEquals(uri, request.getUri().toString());
 
-//        ApiHttpRequest request1 = apiRoot.withProjectKey("test_projectKey").categories().get().withSort("sort").createHttpRequest();
-
-//        Assert.assertEquals("GET", request1.getMethod().toString());
-        Assert.assertEquals("/test_projectKey/categories?sort=sort", request.getUri().toString());
-
-//        ApiHttpRequest request2 = apiRoot.withProjectKey("test_projectKey").categories().get().withLimit(10).createHttpRequest();
-
-//        Assert.assertEquals("GET", request2.getMethod().toString());
-        Assert.assertEquals("/test_projectKey/categories?limit=10", request.getUri().toString());
     }
 
-    private Object[] requestWithParameters() {
+    private Object[] requestWithMethodParameters() {
         return new Object[] {
-                new Object[] { apiRoot.withProjectKey("test_projectKey").categories().get().withExpand("expand").createHttpRequest() },
-                new Object[] { apiRoot.withProjectKey("test_projectKey").categories().get().withSort("sort").createHttpRequest() },
-                new Object[] { apiRoot.withProjectKey("test_projectKey").categories().get().withLimit(10).createHttpRequest() }
+                new Object[] { apiRoot.withProjectKey(projectKey).categories().get().withExpand("expand").createHttpRequest(), "GET", "/test_projectKey/categories?expand=expand"},
+                new Object[] { apiRoot.withProjectKey(projectKey).categories().get().withSort("sort").createHttpRequest(), "GET", "/test_projectKey/categories?sort=sort"},
+                new Object[] { apiRoot.withProjectKey(projectKey).categories().get().withLimit(10).createHttpRequest(), "GET", "/test_projectKey/categories?limit=10" }
         };
     }
 
     @Test
     @Parameters(method = "resourcesParameters")
-    public void resources(ApiHttpRequest request) {
-//        ApiHttpRequest request = apiRoot.withProjectKey("test_projectKey").categories().withKey("test-key").get().createHttpRequest();
-        Assert.assertEquals("/test_projectKey/categories/key=test-key", request.getUri().toString());
+    public void resources(ApiHttpRequest request, String uri) {
 
-//        ApiHttpRequest request1 = apiRoot.withProjectKey("test_projectKey").categories().withId("test-id").get().createHttpRequest();
-        Assert.assertEquals("/test_projectKey/categories/test-id", request.getUri().toString());
+        Assert.assertEquals(uri, request.getUri().toString());
+        Assert.assertEquals(uri, request.getUri().toString());
     }
 
     private Object[] resourcesParameters() {
         return new Object[] {
-                new Object[] { apiRoot.withProjectKey("test_projectKey").categories().withKey("test-key").get().createHttpRequest() },
-                new Object[] { apiRoot.withProjectKey("test_projectKey").categories().withId("test-id").get().createHttpRequest() }
+                new Object[] { apiRoot.withProjectKey(projectKey).categories().withKey("test-key").get().createHttpRequest(), "/test_projectKey/categories/key=test-key" },
+                new Object[] { apiRoot.withProjectKey(projectKey).categories().withId("test-id").get().createHttpRequest(), "/test_projectKey/categories/test-id"}
         };
     }
 
     @Test
     @Parameters(method = "executeMethodParameters")
     public void executeWithNullPointerException(ApiHttpRequest httpRequest) throws Exception{
-//        ApiHttpClient apiHttpClientMock = Mockito.mock(ApiHttpClient.class);
-//        String projectKey = "test-projectkey";
-
-//        ByProjectKeyCategoriesGet byProjectKeyCategoriesGet = new ByProjectKeyCategoriesGet(apiHttpClientMock, projectKey);
-
-//        ApiHttpRequest httpRequest1 = new ApiHttpRequest(ApiHttpMethod.GET, byProjectKeyCategoriesGet.createHttpRequest().getUri(), new ApiHttpHeaders(), null);
         Mockito.when(apiHttpClientMock.execute(httpRequest)).thenThrow(NullPointerException.class);
-
-//        ByProjectKeyCategoriesPost byProjectKeyCategoriesPost = new ByProjectKeyCategoriesPost(apiHttpClientMock, projectKey, CategoryDraft.of());
-
-//        ApiHttpRequest httpRequest2 = new ApiHttpRequest(ApiHttpMethod.POST, byProjectKeyCategoriesPost.createHttpRequest().getUri(), new ApiHttpHeaders(), null);
-//        Mockito.when(apiHttpClientMock.execute(httpRequest2)).thenThrow(NullPointerException.class);
     }
 
     private Object[] executeMethodParameters() {

a24008e1bc2a7dfa3b7cd265b5e7994a1bcc5e6a
Jens Schulze <jens.schulze@commercetools.com>
2021-04-13 21:30:25 +0200

    Restore commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/ReviewTest.java

---
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/ReviewTest.java
@@ -0,0 +1,33 @@
+
+package com.commercetools;
+
+import static com.commercetools.TestUtils.stringFromResource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import com.commercetools.api.models.channel.ChannelReference;
+import com.commercetools.api.models.product.ProductReference;
+import com.commercetools.api.models.review.Review;
+
+import io.vrap.rmf.base.client.utils.json.JsonUtils;
+
+import org.junit.Test;
+
+public class ReviewTest {
+
+    @Test
+    public void productReview() throws IOException {
+        Review review = JsonUtils.fromJsonString(stringFromResource("product-review.json"), Review.class);
+
+        assertThat(review.getTarget()).isInstanceOf(ProductReference.class);
+    }
+
+    @Test
+    public void channelReview() throws IOException {
+        Review review = JsonUtils.fromJsonString(stringFromResource("channel-review.json"), Review.class);
+
+        assertThat(review.getTarget()).isInstanceOf(ChannelReference.class);
+    }
+
+}

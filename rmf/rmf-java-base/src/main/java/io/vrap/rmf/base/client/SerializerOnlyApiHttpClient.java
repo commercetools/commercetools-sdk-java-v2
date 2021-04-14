4b0bdef5c7afe64d8274748e9c2f161b0cd6c46f
Jens Schulze <jenschude@gmail.com>
2021-04-14 10:30:25 +0200

    style fix

--- rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/SerializerOnlyApiHttpClient.java
+++ rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/SerializerOnlyApiHttpClient.java
@@ -1,8 +1,9 @@
+
 package io.vrap.rmf.base.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.concurrent.CompletableFuture;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SerializerOnlyApiHttpClient implements ApiHttpClient {
     public static final String NO_EXECUTABLE_API_CLIENT = "No executable api client";
@@ -24,23 +25,28 @@ public class SerializerOnlyApiHttpClient implements ApiHttpClient {
         return new SerializerOnlyApiHttpClient(serializer);
     }
 
-    @Override public <O> CompletableFuture<ApiHttpResponse<O>> execute(ApiHttpRequest request, Class<O> outputType) {
+    @Override
+    public <O> CompletableFuture<ApiHttpResponse<O>> execute(ApiHttpRequest request, Class<O> outputType) {
         throw new IllegalStateException(NO_EXECUTABLE_API_CLIENT);
     }
 
-    @Override public <O> CompletableFuture<ApiHttpResponse<O>> execute(ClientRequestCommand<O> method) {
+    @Override
+    public <O> CompletableFuture<ApiHttpResponse<O>> execute(ClientRequestCommand<O> method) {
         throw new IllegalStateException(NO_EXECUTABLE_API_CLIENT);
     }
 
-    @Override public ResponseSerializer getSerializerService() {
+    @Override
+    public ResponseSerializer getSerializerService() {
         return serializer;
     }
 
-    @Override public CompletableFuture<ApiHttpResponse<byte[]>> execute(ApiHttpRequest request) {
+    @Override
+    public CompletableFuture<ApiHttpResponse<byte[]>> execute(ApiHttpRequest request) {
         throw new IllegalStateException(NO_EXECUTABLE_API_CLIENT);
     }
 
-    @Override public void close() throws Exception {
+    @Override
+    public void close() throws Exception {
 
     }
 }

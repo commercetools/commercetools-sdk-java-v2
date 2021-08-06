14ae257a8ca5b0fa793f0fb7edce42143b089888
Mauro Pinto <mauro.s.pinto@gmail.com>
2021-08-06 21:10:34 +0100

    add add* methods to PagedQueryResourceRequest

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/PagedQueryResourceRequest.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/PagedQueryResourceRequest.java
@@ -8,15 +8,29 @@ public interface PagedQueryResourceRequest<T extends PagedQueryResourceRequest<T
         extends RequestCommand<TResult>, ClientRequestCommand<TResult> {
     T withLimit(final int limit);
 
+    T addLimit(final int limit);
+
     T withOffset(final int offset);
+
+    T addOffset(final int offset);
 
     T withSort(final String sort);
 
+    T addSort(final String sort);
+
     T withWhere(final String where);
+
+    T addWhere(final String where);
 
     T withExpand(final String expand);
 
+    T addExpand(final String expand);
+
     T withWithTotal(final boolean withTotal);
 
+    T addWithTotal(final boolean withTotal);
+
     T withPredicateVar(final String varName, final String predicateVar);
+
+    T addPredicateVar(final String varName, final String predicateVar);
 }

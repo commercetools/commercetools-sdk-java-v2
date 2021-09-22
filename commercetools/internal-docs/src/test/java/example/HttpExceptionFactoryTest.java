0230c6b0b491c4236493036b49c3fe04e609fd2d
Jens Schulze <jenschude@gmail.com>
2021-09-22 21:37:18 +0200

    fix test

--- commercetools/internal-docs/src/test/java/example/HttpExceptionFactoryTest.java
+++ commercetools/internal-docs/src/test/java/example/HttpExceptionFactoryTest.java
@@ -13,20 +13,21 @@ import org.junit.Test;
 
 public class HttpExceptionFactoryTest {
 
+    static class CustomExceptionFactory implements HttpExceptionFactory {
+        @Override
+        public ApiHttpException create(ApiHttpRequest request, ApiHttpResponse<byte[]> response) {
+            return new ApiHttpException(response.getStatusCode(), "something bad happened", response.getHeaders(),
+                response);
+        }
+
+        @Override
+        public ResponseSerializer getResponseSerializer() {
+            return ResponseSerializer.of();
+        }
+    }
+
     @Test
     public void customFactory() {
-        class CustomExceptionFactory implements HttpExceptionFactory {
-            @Override
-            public ApiHttpException create(ApiHttpRequest request, ApiHttpResponse<byte[]> response) {
-                return new ApiHttpException(response.getStatusCode(), "something bad happened", response.getHeaders(),
-                    response);
-            }
-
-            @Override
-            public ResponseSerializer getResponseSerializer() {
-                return ResponseSerializer.of();
-            }
-        }
 
         ProjectApiRoot client = ApiRootBuilder.of()
                 .defaultClient(ServiceRegion.GCP_EUROPE_WEST1.getApiUrl())

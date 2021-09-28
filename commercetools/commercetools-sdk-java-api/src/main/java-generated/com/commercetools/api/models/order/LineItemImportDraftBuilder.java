96a2d054f07e933318bb0ff970864e573c966675
Auto Mation <automation@commercetools.com>
2021-09-28 05:38:40 +0000

    TASK: Updating SDK

--- commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/order/LineItemImportDraftBuilder.java
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/order/LineItemImportDraftBuilder.java
@@ -21,7 +21,7 @@ public final class LineItemImportDraftBuilder implements Builder<LineItemImportD
 
     private com.commercetools.api.models.common.PriceDraft price;
 
-    private Double quantity;
+    private Long quantity;
 
     @Nullable
     private java.util.List<com.commercetools.api.models.order.ItemState> state;
@@ -80,7 +80,7 @@ public final class LineItemImportDraftBuilder implements Builder<LineItemImportD
         return this;
     }
 
-    public LineItemImportDraftBuilder quantity(final Double quantity) {
+    public LineItemImportDraftBuilder quantity(final Long quantity) {
         this.quantity = quantity;
         return this;
     }
@@ -193,7 +193,7 @@ public final class LineItemImportDraftBuilder implements Builder<LineItemImportD
         return this.price;
     }
 
-    public Double getQuantity() {
+    public Long getQuantity() {
         return this.quantity;
     }
 

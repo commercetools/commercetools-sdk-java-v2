1d70e01a78c20493593f37e0ee91e01c94d5f68a
Jens Schulze <jenschude@gmail.com>
2021-09-28 09:19:16 +0200

    add middleware docs

--- commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/Middlewares.java
+++ commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/Middlewares.java
@@ -1,3 +1,4 @@
+
 package com.commercetools.docs.meta;
 
 /**
@@ -27,9 +28,37 @@ package com.commercetools.docs.meta;
 
     <p>The {@link io.vrap.rmf.base.client.http.RetryMiddleware} is used to retry a failed request returns with the configured
     status codes e.g. <code>503</code>. For retrying an exponential backoff strategy is used which will increase the wait
-    time before each try to avoid overwhelming the API. An jitter is also added to the wait time so that the executor is
-    not overwhelmed with multiple parallel request.</p>
+    time before each try to avoid overwhelming the API. A jitter is also added to avoid reduce the possibility that parallel
+    requests will be retried at the same time.</p>
 
+    <p>The retry implementation uses the library <a href="https://failsafe.dev/">failsafe</a></p>
+
+    <h3>UserAgentMiddleware</h3>
+
+    <p>The {@link io.vrap.rmf.base.client.http.UserAgentMiddleware} adds an user agent header to every request. By default
+    is submits the following data:</p>
+    <ul>
+        <li>SDK version</li>
+        <li>JVM runtime version</li>
+        <li>Operating system name and version</li>
+    </ul>
+
+    {@include.example io.vrap.rmf.base.client.ClientBuilder#buildDefaultUserAgent()}
+
+    <h3>AcceptGZipMiddleware</h3>
+
+    <p>The {@link io.vrap.rmf.base.client.http.AcceptGZipMiddleware} adds an <code>Accept-Encoding: gzip</code> header, to
+    enable the ability of an compressed response as this reduces the data transport time.</p>
+
+    {@include.example io.vrap.rmf.base.client.http.AcceptGZipMiddleware#invoke(io.vrap.rmf.base.client.ApiHttpRequest, java.util.function.Function)}
+
+    <h3>InternalLoggerMiddleware</h3>
+
+    <p>The {@link io.vrap.rmf.base.client.http.InternalLoggerMiddleware}</p> logs every request send using the SDK. Depending
+    on the configured logging level the default implementation logs the sent request and the returning response. The info level
+    outputs the request method, url and the response status code. Using debug level the request and response instance will be logged.
+    With trace level it will pretty print body of the request as well the response. To avoid leaking of sensitive data the
+    logger redacts passwords, tokens and some other sensitive data.
  */
 public class Middlewares {
 }

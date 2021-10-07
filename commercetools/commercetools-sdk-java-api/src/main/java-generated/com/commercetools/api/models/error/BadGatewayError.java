bd135b6ee8ac4493f12281397ebb3736e2ded764
Auto Mation <automation@commercetools.com>
2021-10-07 12:21:02 +0000

    TASK: Updating SDK

---
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/error/BadGatewayError.java
@@ -0,0 +1,40 @@
+
+package com.commercetools.api.models.error;
+
+import java.time.*;
+import java.util.*;
+import java.util.function.Function;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.*;
+
+import io.vrap.rmf.base.client.utils.Generated;
+
+@Generated(value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator", comments = "https://github.com/vrapio/rmf-codegen")
+@JsonDeserialize(as = BadGatewayErrorImpl.class)
+public interface BadGatewayError extends ErrorObject {
+
+    String BAD_GATEWAY = "BadGateway";
+
+    public static BadGatewayError of() {
+        return new BadGatewayErrorImpl();
+    }
+
+    public static BadGatewayError of(final BadGatewayError template) {
+        BadGatewayErrorImpl instance = new BadGatewayErrorImpl();
+        instance.setMessage(template.getMessage());
+        return instance;
+    }
+
+    public static BadGatewayErrorBuilder builder() {
+        return BadGatewayErrorBuilder.of();
+    }
+
+    public static BadGatewayErrorBuilder builder(final BadGatewayError template) {
+        return BadGatewayErrorBuilder.of(template);
+    }
+
+    default <T> T withBadGatewayError(Function<BadGatewayError, T> helper) {
+        return helper.apply(this);
+    }
+}
---
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/error/BadGatewayErrorBuilder.java
@@ -0,0 +1,45 @@
+
+package com.commercetools.api.models.error;
+
+import java.util.*;
+
+import io.vrap.rmf.base.client.Builder;
+import io.vrap.rmf.base.client.utils.Generated;
+
+@Generated(value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator", comments = "https://github.com/vrapio/rmf-codegen")
+public final class BadGatewayErrorBuilder implements Builder<BadGatewayError> {
+
+    private String message;
+
+    public BadGatewayErrorBuilder message(final String message) {
+        this.message = message;
+        return this;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public BadGatewayError build() {
+        Objects.requireNonNull(message, BadGatewayError.class + ": message is missing");
+        return new BadGatewayErrorImpl(message);
+    }
+
+    /**
+     * builds BadGatewayError without checking for non null required values
+     */
+    public BadGatewayError buildUnchecked() {
+        return new BadGatewayErrorImpl(message);
+    }
+
+    public static BadGatewayErrorBuilder of() {
+        return new BadGatewayErrorBuilder();
+    }
+
+    public static BadGatewayErrorBuilder of(final BadGatewayError template) {
+        BadGatewayErrorBuilder builder = new BadGatewayErrorBuilder();
+        builder.message = template.getMessage();
+        return builder;
+    }
+
+}

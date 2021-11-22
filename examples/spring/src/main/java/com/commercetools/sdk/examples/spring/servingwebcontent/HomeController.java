ac234c394f417f355a59ace34df4248a4a925053
Jens Schulze <jens.schulze@commercetools.com>
2021-11-22 14:53:08 +0100

    add spring example

---
+++ examples/spring/src/main/java/com/commercetools/sdk/examples/spring/servingwebcontent/HomeController.java
@@ -0,0 +1,12 @@
+package com.commercetools.sdk.examples.spring.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ examples/spring/src/test/java/com/commercetools/sdk/examples/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercetools.sdk.examples.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

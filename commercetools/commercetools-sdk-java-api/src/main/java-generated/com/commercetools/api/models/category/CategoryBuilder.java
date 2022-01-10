d093b047ca2ca9e6ba57c1c1979fd281784cab98
Jens Schulze <jens.schulze@commercetools.com>
2022-01-10 20:33:24 +0100

    add license check

--- commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/category/CategoryBuilder.java
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/category/CategoryBuilder.java
@@ -4,10 +4,11 @@ package com.commercetools.api.models.category;
 import java.util.*;
 import java.util.function.Function;
 
-import javax.annotation.Nullable;
 
 import io.vrap.rmf.base.client.Builder;
 import io.vrap.rmf.base.client.utils.Generated;
+
+import javax.annotation.Nullable;
 
 @Generated(value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator", comments = "https://github.com/vrapio/rmf-codegen")
 public class CategoryBuilder implements Builder<Category> {

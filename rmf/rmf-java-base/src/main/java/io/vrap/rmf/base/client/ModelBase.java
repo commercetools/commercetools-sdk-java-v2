b7e267ecc15943f769dc0c4e75e8287278797e2a
Jens Schulze <jens.schulze@commercetools.com>
2022-01-19 20:42:33 +0100

    add model base class

---
+++ rmf/rmf-java-base/src/main/java/io/vrap/rmf/base/client/ModelBase.java
@@ -0,0 +1,28 @@
+
+package io.vrap.rmf.base.client;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+abstract class ModelBase {
+
+    @Override
+    abstract public boolean equals(final Object o);
+
+    @Override
+    abstract public int hashCode();
+
+    public String reflectionString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    protected boolean reflectionEquals(final Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    public int reflectionHashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}

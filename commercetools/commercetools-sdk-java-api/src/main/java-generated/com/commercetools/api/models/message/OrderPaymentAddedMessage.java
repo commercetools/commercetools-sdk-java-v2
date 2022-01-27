dfd504925edd88640876cc20def387d770c0158c
Auto Mation <automation@commercetools.com>
2022-01-27 10:26:49 +0000

    TASK: Updating SDK

---
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/message/OrderPaymentAddedMessage.java
@@ -0,0 +1,61 @@
+
+package com.commercetools.api.models.message;
+
+import java.time.*;
+import java.util.*;
+import java.util.function.Function;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.commercetools.api.models.payment.PaymentReference;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.*;
+
+import io.vrap.rmf.base.client.utils.Generated;
+
+@Generated(value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator", comments = "https://github.com/vrapio/rmf-codegen")
+@JsonDeserialize(as = OrderPaymentAddedMessageImpl.class)
+public interface OrderPaymentAddedMessage extends Message {
+
+    String ORDER_PAYMENT_ADDED = "OrderPaymentAdded";
+
+    @NotNull
+    @Valid
+    @JsonProperty("payment")
+    public PaymentReference getPayment();
+
+    public void setPayment(final PaymentReference payment);
+
+    public static OrderPaymentAddedMessage of() {
+        return new OrderPaymentAddedMessageImpl();
+    }
+
+    public static OrderPaymentAddedMessage of(final OrderPaymentAddedMessage template) {
+        OrderPaymentAddedMessageImpl instance = new OrderPaymentAddedMessageImpl();
+        instance.setId(template.getId());
+        instance.setVersion(template.getVersion());
+        instance.setCreatedAt(template.getCreatedAt());
+        instance.setLastModifiedAt(template.getLastModifiedAt());
+        instance.setLastModifiedBy(template.getLastModifiedBy());
+        instance.setCreatedBy(template.getCreatedBy());
+        instance.setSequenceNumber(template.getSequenceNumber());
+        instance.setResource(template.getResource());
+        instance.setResourceVersion(template.getResourceVersion());
+        instance.setResourceUserProvidedIdentifiers(template.getResourceUserProvidedIdentifiers());
+        instance.setPayment(template.getPayment());
+        return instance;
+    }
+
+    public static OrderPaymentAddedMessageBuilder builder() {
+        return OrderPaymentAddedMessageBuilder.of();
+    }
+
+    public static OrderPaymentAddedMessageBuilder builder(final OrderPaymentAddedMessage template) {
+        return OrderPaymentAddedMessageBuilder.of(template);
+    }
+
+    default <T> T withOrderPaymentAddedMessage(Function<OrderPaymentAddedMessage, T> helper) {
+        return helper.apply(this);
+    }
+}
---
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/message/OrderPaymentAddedMessagePayloadImpl.java
@@ -0,0 +1,65 @@
+
+package com.commercetools.api.models.message;
+
+import java.time.*;
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.*;
+
+import io.vrap.rmf.base.client.ModelBase;
+import io.vrap.rmf.base.client.utils.Generated;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+@Generated(value = "io.vrap.rmf.codegen.rendring.CoreCodeGenerator", comments = "https://github.com/vrapio/rmf-codegen")
+public class OrderPaymentAddedMessagePayloadImpl implements OrderPaymentAddedMessagePayload, ModelBase {
+
+    private String type;
+
+    private com.commercetools.api.models.payment.PaymentReference payment;
+
+    @JsonCreator
+    OrderPaymentAddedMessagePayloadImpl(
+            @JsonProperty("payment") final com.commercetools.api.models.payment.PaymentReference payment) {
+        this.payment = payment;
+        this.type = ORDER_PAYMENT_ADDED;
+    }
+
+    public OrderPaymentAddedMessagePayloadImpl() {
+        this.type = ORDER_PAYMENT_ADDED;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public com.commercetools.api.models.payment.PaymentReference getPayment() {
+        return this.payment;
+    }
+
+    public void setPayment(final com.commercetools.api.models.payment.PaymentReference payment) {
+        this.payment = payment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        OrderPaymentAddedMessagePayloadImpl that = (OrderPaymentAddedMessagePayloadImpl) o;
+
+        return new EqualsBuilder().append(type, that.type).append(payment, that.payment).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(type).append(payment).toHashCode();
+    }
+
+}

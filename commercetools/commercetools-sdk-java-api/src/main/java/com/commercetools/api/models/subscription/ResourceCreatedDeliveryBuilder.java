44737b756dd325f8af09fa5ba13b6f8509977451
Jens Schulze <jens.schulze@commercetools.com>
2022-02-09 15:13:01 +0100

    add missing deprecation classes/interfaces

---
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/subscription/ResourceCreatedDeliveryBuilder.java
@@ -0,0 +1,11 @@
+package com.commercetools.api.models.subscription;
+
+import com.commercetools.api.Replaced;
+
+/**
+ * @deprecated use {@link ResourceCreatedDeliveryPayloadBuilder} instead
+ */
+@Deprecated
+@Replaced(by = ResourceCreatedDeliveryPayloadBuilder.class)
+public class ResourceCreatedDeliveryBuilder extends ResourceCreatedDeliveryPayloadBuilder {
+}

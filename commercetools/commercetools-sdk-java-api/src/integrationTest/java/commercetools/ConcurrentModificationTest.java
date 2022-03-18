77d8b79e6fd0a843fd4939ce4a6deb2eb487c2e7
Jens Schulze <jens.schulze@commercetools.com>
2022-03-18 10:34:15 +0100

    add concurrent modification middleware

--- commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/ConcurrentModificationTest.java
+++ commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/ConcurrentModificationTest.java
@@ -1,8 +1,11 @@
 
 package commercetools;
 
+import com.commercetools.api.client.ConcurrentModificationMiddleware;
 import com.commercetools.api.client.ProjectApiRoot;
 import com.commercetools.api.client.RetryHandler;
+import com.commercetools.api.defaultconfig.ApiRootBuilder;
+import com.commercetools.api.defaultconfig.ServiceRegion;
 import com.commercetools.api.models.cart.Cart;
 import com.commercetools.api.models.cart.CartSetCountryActionBuilder;
 import com.commercetools.api.models.cart.CartUpdate;
@@ -11,6 +14,7 @@ import commercetools.cart.CartsFixtures;
 import commercetools.utils.CommercetoolsTestUtils;
 
 import io.vrap.rmf.base.client.ApiHttpResponse;
+import io.vrap.rmf.base.client.oauth2.ClientCredentials;
 
 import org.junit.jupiter.api.Test;
 
@@ -36,6 +40,41 @@ public class ConcurrentModificationTest {
                                     .actions(CartSetCountryActionBuilder.of().country("DE").build())
                                     .build()),
                         CartUpdate::builder, CartUpdateBuilder::version)
+                    .executeBlocking()
+                    .getBody();
+            return modCart;
+        });
+    }
+
+    @Test
+    public void concurrentModMiddleware() {
+        String projectKey = CommercetoolsTestUtils.getProjectKey();
+
+        ProjectApiRoot projectApiRoot = ApiRootBuilder.of()
+                .defaultClient(ClientCredentials.of()
+                        .withClientId(CommercetoolsTestUtils.getClientId())
+                        .withClientSecret(CommercetoolsTestUtils.getClientSecret())
+                        .build(),
+                    ServiceRegion.GCP_EUROPE_WEST1)
+                .addMiddleware(new ConcurrentModificationMiddleware(3))
+                .build(projectKey);
+
+        CartsFixtures.withUpdateableCart(cart -> {
+
+            final ApiHttpResponse<Cart> deCart = projectApiRoot.carts()
+                    .withId(cart.getId())
+                    .post(CartUpdateBuilder.of()
+                            .version(cart.getVersion())
+                            .actions(CartSetCountryActionBuilder.of().country("DE").build())
+                            .build())
+                    .executeBlocking();
+
+            final Cart modCart = projectApiRoot.carts()
+                    .withId(cart.getId())
+                    .post(CartUpdateBuilder.of()
+                            .version(cart.getVersion())
+                            .actions(CartSetCountryActionBuilder.of().country("DE").build())
+                            .build())
                     .executeBlocking()
                     .getBody();
             return modCart;

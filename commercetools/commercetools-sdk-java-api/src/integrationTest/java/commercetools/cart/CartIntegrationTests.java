29031853a94a6466fa6c667d74f7fb2f6a4e0311
Jens Schulze <jens.schulze@commercetools.com>
2022-03-23 10:35:10 +0100

    query carts test

--- commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/cart/CartIntegrationTests.java
+++ commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/cart/CartIntegrationTests.java
@@ -1,16 +1,13 @@
 package commercetools.cart;
 
 import com.commercetools.api.models.cart.Cart;
-import commercetools.customer.CustomerFixtures;
 import commercetools.utils.CommercetoolsTestUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 public class CartIntegrationTests {
     @Test
-    public void getByCustomerId() {
+    public void queryByCustomerId() {
         CartsFixtures.withCartWithCustomer((cart, customer) -> {
             Cart queriedCart = CommercetoolsTestUtils.getProjectApiRoot()
                     .carts()
@@ -21,6 +18,31 @@ public class CartIntegrationTests {
                     .getResults()
                     .get(0);
             Assertions.assertThat(queriedCart.getId()).isEqualTo(cart.getId());
+
+            Cart queriedCart2 = CommercetoolsTestUtils.getProjectApiRoot()
+                    .carts()
+                    .get()
+                    .withWhere("customerId = :customerId and cartState = \"Active\"")
+                    .withPredicateVar("customerId", customer.getId())
+                    .executeBlocking()
+                    .getBody()
+                    .getResults()
+                    .get(0);
+            Assertions.assertThat(queriedCart2.getId()).isEqualTo(cart.getId());
         });
     }
+
+    @Test
+    public void getByCustomerId() {
+        CartsFixtures.withCartWithCustomer((cart, customer) -> {
+            Cart queriedCart = CommercetoolsTestUtils.getProjectApiRoot()
+                    .carts()
+                    .withCustomerId(customer.getId())
+                    .get()
+                    .executeBlocking()
+                    .getBody();
+            Assertions.assertThat(queriedCart.getId()).isEqualTo(cart.getId());
+        });
+    }
+
 }

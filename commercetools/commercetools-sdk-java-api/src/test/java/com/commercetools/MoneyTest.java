a0584e6f32ad7b9fb400383df4c5017c21302c0e
Jens Schulze <jens.schulze@commercetools.com>
2022-04-05 12:21:21 +0200

    add example for conversion of Money to Moneta

---
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/MoneyTest.java
@@ -0,0 +1,35 @@
+package com.commercetools;
+
+import com.commercetools.api.models.common.*;
+import org.assertj.core.api.Assertions;
+import org.javamoney.moneta.FastMoney;
+import org.junit.jupiter.api.Test;
+
+import javax.money.MonetaryAmount;
+
+public class MoneyTest {
+    @Test
+    public void testMonetaConversion() {
+        final CentPrecisionMoney centPrecisionMoney = CentPrecisionMoneyBuilder.of().centAmount(100L).fractionDigits(2).currencyCode("EUR").build();
+        final MonetaryAmount centAmount = centPrecisionMoney.withCentPrecisionMoney(
+                money -> FastMoney.of(money.getCentAmount() / Math.pow(10, money.getFractionDigits()), money.getCurrencyCode()));
+        Assertions.assertThat(centAmount.getNumber().doubleValueExact()).isEqualTo(1.00);
+        Assertions.assertThat(centAmount.isZero()).isFalse();
+
+        final HighPrecisionMoney highPrecisionMoney = HighPrecisionMoneyBuilder.of().centAmount(100L).preciseAmount(1001L).fractionDigits(3).currencyCode("EUR").build();
+        final MonetaryAmount highAmount = highPrecisionMoney.withHighPrecisionMoney(
+                money -> FastMoney.of((money.getPreciseAmount() / Math.pow(10, money.getFractionDigits())), money.getCurrencyCode()));
+        Assertions.assertThat(highAmount.getNumber().doubleValueExact()).isEqualTo(1.001);
+
+        final TypedMoney typedMoney = HighPrecisionMoneyBuilder.of().centAmount(100L).preciseAmount(1001L).fractionDigits(3).currencyCode("EUR").build();
+        final MonetaryAmount typedAmount = typedMoney.withTypedMoney(
+                money -> {
+                    if (money instanceof HighPrecisionMoney) {
+                        return FastMoney.of(((HighPrecisionMoney) money).getPreciseAmount() / Math.pow(10, money.getFractionDigits()), money.getCurrencyCode());
+                    }
+                    return FastMoney.of(money.getCentAmount() / Math.pow(10, money.getFractionDigits()), money.getCurrencyCode());
+                });
+        Assertions.assertThat(typedAmount.getNumber().doubleValueExact()).isEqualTo(1.001);
+
+    }
+}

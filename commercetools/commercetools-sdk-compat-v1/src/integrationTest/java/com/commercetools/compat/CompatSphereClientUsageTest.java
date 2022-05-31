3e44651b865b1ed6b4745c2a4df812ab0d585db7
Jens Schulze <jens.schulze@commercetools.com>
2022-05-31 13:51:59 +0200

    add a doc note for compat mode

--- commercetools/commercetools-sdk-compat-v1/src/integrationTest/java/com/commercetools/compat/CompatSphereClientUsageTest.java
+++ commercetools/commercetools-sdk-compat-v1/src/integrationTest/java/com/commercetools/compat/CompatSphereClientUsageTest.java
@@ -3,20 +3,30 @@ package com.commercetools.compat;
 
 import java.util.concurrent.ExecutionException;
 
+import com.commercetools.api.defaultconfig.ApiRootBuilder;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import io.sphere.sdk.projects.Project;
 import io.sphere.sdk.projects.queries.ProjectGet;
 
+import io.vrap.rmf.base.client.ApiHttpClient;
+import io.vrap.rmf.base.client.oauth2.ClientCredentials;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CompatSphereClientUsageTest {
     @Test
     public void sphereCompatClient() throws ExecutionException, InterruptedException {
+        final ApiHttpClient apiHttpClient = ApiRootBuilder.of()
+                .defaultClient(ClientCredentials.of()
+                        .withClientId(CommercetoolsTestUtils.getClientId())
+                        .withClientSecret(CommercetoolsTestUtils.getClientSecret())
+                        .build())
+                .buildClient();
+
         SphereClientConfig clientConfig = SphereClientConfig.of(CommercetoolsTestUtils.getProjectKey(),
             CommercetoolsTestUtils.getClientId(), CommercetoolsTestUtils.getClientSecret());
-        SphereClient client = CompatSphereClient.of(clientConfig);
+        SphereClient client = CompatSphereClient.of(apiHttpClient, clientConfig);
 
         Project project = client.execute(ProjectGet.of()).toCompletableFuture().get();
 
--- commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/Compatibility.java
+++ commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/Compatibility.java
@@ -6,6 +6,12 @@ package com.commercetools.docs.meta;
  *
  * <p>The module <code>commercetools-sdk-compat-v1</code> allows the usage of the SDK in conjunction with the JVM SDK (v1)</p>
  *
+ * <h3>Creating a SphereClient with the v2 SDK</h3>
+ *
+ * <p>The {@link com.commercetools.compat.CompatSphereClient} acts as a replacement {@link io.sphere.sdk.client.SphereClient} for applications using the JVM SDK (v1).</p>
+ *
+ * {@include.example com.commercetools.compat.CompatSphereClientUsageTest}
+ *
  * <h3>Using Requests from the JVM SDK (v1)</h3>
  *
  * <p>The {@link com.commercetools.compat.CompatClient} can be used to execute a {@link io.sphere.sdk.client.SphereRequest}
@@ -13,11 +19,6 @@ package com.commercetools.docs.meta;
  *
  * {@include.example com.commercetools.compat.CompatClientUsageTest}
  *
- * <h3>Creating a SphereClient with the v2 SDK</h3>
- *
- * <p>The {@link com.commercetools.compat.CompatSphereClient} acts as a replacement {@link io.sphere.sdk.client.SphereClient} for applications using the JVM SDK (v1).</p>
- *
- * {@include.example com.commercetools.compat.CompatSphereClientUsageTest}
  */
 public class Compatibility {
 }

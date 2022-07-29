5a2eba265862aefcfce03965ff051b9ecfa65d94
Jens Schulze <jens.schulze@commercetools.com>
2022-07-29 13:59:20 +0200

    fix ProductDataLike serialization

--- commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/ProductTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/ProductTest.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.commercetools.api.models.common.LocalizedStringBuilder;
-import com.commercetools.api.models.product.ProductDraft;
-import com.commercetools.api.models.product.ProductDraftBuilder;
-import com.commercetools.api.models.product.ProductVariantDraft;
-import com.commercetools.api.models.product.ProductVariantDraftBuilder;
+import com.commercetools.api.models.product.*;
 import com.commercetools.api.models.product_type.ProductTypeResourceIdentifierBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.vrap.rmf.base.client.utils.json.JsonUtils;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -36,5 +36,16 @@ public class ProductTest {
         Assertions.assertThat(draft.getMasterVariant().getSku()).isEqualTo("abc");
         Assertions.assertThat(draft.getVariants()).hasSize(1);
         Assertions.assertThat(draft.getVariants().get(0).getSku()).isEqualTo("def");
+    }
+
+    @Test
+    public void serializeProduct() throws JsonProcessingException {
+        final ProductProjection productProjection = ProductProjection.builder()
+                .masterVariant(b -> b.sku("foo").id(1L))
+                .plusVariants(b -> b.sku("bar").id(2L))
+                .buildUnchecked();
+
+        Assertions.assertThat(JsonUtils.toJsonString(productProjection))
+                .isEqualTo("{\"masterVariant\":{\"id\":1,\"sku\":\"foo\"},\"variants\":[{\"id\":2,\"sku\":\"bar\"}]}");
     }
 }

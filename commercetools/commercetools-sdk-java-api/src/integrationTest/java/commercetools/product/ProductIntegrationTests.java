3c5e6c6d02c261d59e918938d5fedab96b84c9b6
Auto Mation <automation@commercetools.com>
2022-08-04 10:43:46 +0000

    spotless: Fix code style

--- commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/product/ProductIntegrationTests.java
+++ commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/product/ProductIntegrationTests.java
@@ -120,18 +120,20 @@ public class ProductIntegrationTests {
     @Test
     public void searchBySku() {
         withTaxCategory(
-                taxCategory -> withCategory(category -> withProductType(createProductTypeDraft(), productType -> {
-                    Product product = createProduct(productType, category, taxCategory);
-            ProductProjectionPagedSearchResponse response = CommercetoolsTestUtils.getProjectApiRoot()
-                    .productProjections()
-                    .search()
-                    .get().withStaged(true)
-                    .withQueryParam("sku", product.getMasterData().getCurrent().getMasterVariant().getSku() )
-                    .withLimit(1)
-                    .executeBlocking()
-                    .getBody();
-            Assertions.assertEquals(response.getResults().get(0).getMasterVariant().getSku(), product.getMasterData().getCurrent().getMasterVariant().getSku());
-        })));
+            taxCategory -> withCategory(category -> withProductType(createProductTypeDraft(), productType -> {
+                Product product = createProduct(productType, category, taxCategory);
+                ProductProjectionPagedSearchResponse response = CommercetoolsTestUtils.getProjectApiRoot()
+                        .productProjections()
+                        .search()
+                        .get()
+                        .withStaged(true)
+                        .withQueryParam("sku", product.getMasterData().getCurrent().getMasterVariant().getSku())
+                        .withLimit(1)
+                        .executeBlocking()
+                        .getBody();
+                Assertions.assertEquals(response.getResults().get(0).getMasterVariant().getSku(),
+                    product.getMasterData().getCurrent().getMasterVariant().getSku());
+            })));
     }
 
     @Test

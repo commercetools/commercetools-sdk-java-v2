0c25bf0f3831e19f3608e2f2e1114972a629e327
Jens Schulze <jens.schulze@commercetools.com>
2023-01-25 12:34:21 +0100

    add benchmark to retrieve products

--- commercetools/commercetools-sdk-compat-v1/src/jmh/java/com/commercetools/benchmark/ClientBenchmark.java
+++ commercetools/commercetools-sdk-compat-v1/src/jmh/java/com/commercetools/benchmark/ClientBenchmark.java
@@ -2,10 +2,19 @@
 package com.commercetools.benchmark;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import com.commercetools.api.client.ApiInternalLoggerFactory;
 import com.commercetools.api.client.ProjectApiRoot;
 import com.commercetools.api.defaultconfig.ApiRootBuilder;
 import com.commercetools.api.defaultconfig.ServiceRegion;
+import com.commercetools.api.models.common.LocalizedString;
+import com.commercetools.api.models.product.Product;
+import com.commercetools.api.models.product_type.AttributeTypeBuilder;
+import com.commercetools.api.models.product_type.ProductType;
+import com.commercetools.api.models.tax_category.TaxCategory;
 import com.commercetools.compat.CompatSphereClient;
 import com.commercetools.http.apachehttp.CtApacheHttpClient;
 import com.commercetools.http.asynchttp.CtAsyncHttpClient;
@@ -14,6 +23,7 @@ import com.commercetools.http.okhttp4.CtOkHttp4Client;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientFactory;
+import io.sphere.sdk.products.queries.ProductProjectionQuery;
 import io.sphere.sdk.projects.queries.ProjectGet;
 import io.vrap.rmf.base.client.oauth2.ClientCredentials;
 
@@ -32,24 +42,36 @@ public class ClientBenchmark {
 
         private BlockingSphereClient compatClient;
 
+        private List<Product> productsList;
+
+        private TaxCategory taxCategory;
+
+        private ProductType productType;
+
         @Setup(Level.Trial)
         public void init() {
             ahcApiRoot = ApiRootBuilder.of(new CtAsyncHttpClient())
                     .defaultClient(
                         ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
                         ServiceRegion.GCP_EUROPE_WEST1)
+                    .withInternalLoggerFactory(ApiInternalLoggerFactory::get, org.slf4j.event.Level.DEBUG,
+                        org.slf4j.event.Level.DEBUG)
                     .build(getProjectKey());
 
             apacheApiRoot = ApiRootBuilder.of(new CtApacheHttpClient())
                     .defaultClient(
                         ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
                         ServiceRegion.GCP_EUROPE_WEST1)
+                    .withInternalLoggerFactory(ApiInternalLoggerFactory::get, org.slf4j.event.Level.DEBUG,
+                        org.slf4j.event.Level.DEBUG)
                     .build(getProjectKey());
 
             okhttpApiRoot = ApiRootBuilder.of(new CtOkHttp4Client())
                     .defaultClient(
                         ClientCredentials.of().withClientId(getClientId()).withClientSecret(getClientSecret()).build(),
                         ServiceRegion.GCP_EUROPE_WEST1)
+                    .withInternalLoggerFactory(ApiInternalLoggerFactory::get, org.slf4j.event.Level.DEBUG,
+                        org.slf4j.event.Level.DEBUG)
                     .build(getProjectKey());
 
             final SphereClientFactory factory = SphereClientFactory.of();
@@ -60,15 +82,95 @@ public class ClientBenchmark {
             sphereClient = BlockingSphereClient.of(client, Duration.ofSeconds(10));
 
             compatClient = BlockingSphereClient.of(CompatSphereClient.of(ahcApiRoot), Duration.ofSeconds(10));
+
+            productType = createProductType();
+
+            taxCategory = createTaxCategory();
+            productsList = createProducts(taxCategory, productType);
         }
 
         @TearDown(Level.Trial)
         public void tearDown() {
+            deleteProducts(productsList);
+            deleteProductType(productType);
+            deleteTaxCategory(taxCategory);
             ahcApiRoot.close();
             compatClient.close();
             apacheApiRoot.close();
             okhttpApiRoot.close();
             sphereClient.close();
+        }
+
+        public ProductType createProductType() {
+            String suffix = randomKey();
+            return ahcApiRoot.productTypes()
+                    .create(builder -> builder.key("benchmark-" + suffix)
+                            .name("benchmark")
+                            .description("benchmark")
+                            .plusAttributes(attributeBuilder -> attributeBuilder.type(AttributeTypeBuilder::textBuilder)
+                                    .isRequired(false)
+                                    .name("benchmark-color")
+                                    .isSearchable(true)
+                                    .label(LocalizedString.ofEnglish("color"))))
+                    .executeBlocking()
+                    .getBody();
+        }
+
+        private TaxCategory createTaxCategory() {
+            String suffix = randomKey();
+            return ahcApiRoot.taxCategories()
+                    .create(builder -> builder.key("benchmark-" + suffix)
+                            .name("benchmark-" + suffix)
+                            .description("benchmark-" + suffix)
+                            .plusRates(rateBuilder -> rateBuilder.name("Mwst")
+                                    .country("DE")
+                                    .amount(0.19)
+                                    .includedInPrice(true)))
+                    .executeBlocking()
+                    .getBody();
+        }
+
+        private List<Product> createProducts(TaxCategory taxCategory, ProductType productType) {
+            String productSuffix = randomKey();
+            List<Product> products = new ArrayList<>();
+            for (int i = 0; i < 150; i++) {
+                String suffix = productSuffix + "-" + i;
+                Product product = ahcApiRoot.products()
+                        .create(
+                            builder -> builder.key("benchmark-" + suffix)
+                                    .productType(p -> p.id(productType.getId()))
+                                    .taxCategory(t -> t.id(taxCategory.getId()))
+                                    .name(LocalizedString.ofEnglish("benchmark-" + suffix))
+                                    .slug(LocalizedString.ofEnglish("benchmark-" + suffix))
+                                    .description(LocalizedString.ofEnglish("benchmark-" + suffix))
+                                    .masterVariant(variantBuilder -> variantBuilder.key("benchmark-variant1-" + suffix)
+                                            .sku("benchmark-variant1-" + suffix)
+                                            .plusPrices(price -> price.country("DE")
+                                                    .value(moneyBuilder -> moneyBuilder.centAmount(100L)
+                                                            .currencyCode("EUR")))
+                                            .plusAttributes(
+                                                attribute -> attribute.name("benchmark-color").value("red" + suffix))))
+                        .executeBlocking()
+                        .getBody();
+                products.add(product);
+            }
+            return products;
+        }
+
+        private void deleteProducts(List<Product> products) {
+            products.forEach(product -> ahcApiRoot.products().delete(product).executeBlocking());
+        }
+
+        private void deleteProductType(ProductType productType) {
+            ahcApiRoot.productTypes().delete(productType).executeBlocking();
+        }
+
+        private void deleteTaxCategory(TaxCategory taxCategory) {
+            ahcApiRoot.taxCategories().delete(taxCategory).executeBlocking();
+        }
+
+        public static String randomKey() {
+            return "random-key-" + UUID.randomUUID().toString();
         }
     }
 
@@ -95,6 +197,16 @@ public class ClientBenchmark {
     @Benchmark
     public void retrieveProjectV2_Compat(ClientState state) {
         state.compatClient.executeBlocking(ProjectGet.of());
+    }
+
+    @Benchmark
+    public void retrieveProductsV2_Compat(ClientState state) {
+        state.compatClient.executeBlocking(ProductProjectionQuery.ofStaged().withLimit(100));
+    }
+
+    @Benchmark
+    public void retrieveProductsV1_AHC(ClientState state) {
+        state.sphereClient.executeBlocking(ProductProjectionQuery.ofStaged().withLimit(100));
     }
 
     public static String getProjectKey() {

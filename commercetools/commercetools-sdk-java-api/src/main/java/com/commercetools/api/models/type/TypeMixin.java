4705bd557e0021a5aafdeda695f9b571b49ae5db
Barbara Palumbo <barbara.palumbo@commercetools.de>
2023-02-17 12:26:09 +0100

    add methods for now throught the main mixin file

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/type/TypeMixin.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/type/TypeMixin.java
@@ -4,10 +4,17 @@ package com.commercetools.api.models.type;
 import com.commercetools.api.models.Referencable;
 import com.commercetools.api.models.ResourceIdentifiable;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public interface TypeMixin extends Referencable<Type>, ResourceIdentifiable<Type> {
     List<FieldDefinition> getFieldDefinitions();
+
+    @Nullable
+    default FieldDefinition getFieldDefinitionByName(final String name) {
+        return getFieldDefinitions().stream().filter(def -> def.getName().equals(name)).findFirst().orElse(null);
+    }
+
     @Override
     public default TypeResourceIdentifier toResourceIdentifier() {
         return TypeResourceIdentifier.builder().id(getId()).build();
@@ -18,8 +25,4 @@ public interface TypeMixin extends Referencable<Type>, ResourceIdentifiable<Type
         return TypeReference.builder().id(getId()).build();
     }
 
-    @javax.annotation.Nullable
-    default FieldDefinition getFieldDefinitionByName(final String name) {
-        return getFieldDefinitions().stream().filter(def -> def.getName().equals(name)).findFirst().orElse(null);
-    }
 }
--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/zone/ZoneMixin.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/zone/ZoneMixin.java
@@ -4,7 +4,16 @@ package com.commercetools.api.models.zone;
 import com.commercetools.api.models.Referencable;
 import com.commercetools.api.models.ResourceIdentifiable;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ZoneMixin extends Referencable<Zone>, ResourceIdentifiable<Zone> {
+    List<Location> getLocations();
+
+    default boolean contains(final String countryCode) {
+        return getLocations().stream().anyMatch(location -> location.getCountry().equals(countryCode));
+    }
+
     @Override
     public default ZoneResourceIdentifier toResourceIdentifier() {
         return ZoneResourceIdentifier.builder().id(getId()).build();

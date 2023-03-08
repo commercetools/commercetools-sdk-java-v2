39c458dc29fa5e8fcdd1cc5e8b13b8940fa61e57
lojzatran <lam.tran@commercetools.com>
2023-03-08 11:52:35 +0000

    Add tests for order

---
+++ commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/order/OrderQueryTests.java
@@ -0,0 +1,37 @@
+package commercetools.order;
+
+import com.commercetools.api.models.order.Order;
+import commercetools.utils.CommercetoolsTestUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderQueryTests {
+
+    @Test
+    public void getById(){
+        OrdersFixtures.withOrder(order -> {
+            final Order queriedOrder = CommercetoolsTestUtils.getProjectApiRoot()
+                    .orders()
+                    .withId(order.getId())
+                    .get()
+                    .executeBlocking()
+                    .getBody();
+
+            Assertions.assertEquals(order.getId(), queriedOrder.getId());
+        });
+    }
+
+    @Test
+    public void getByOrderNumber(){
+        OrdersFixtures.withOrder(order -> {
+            final Order queriedOrder = CommercetoolsTestUtils.getProjectApiRoot()
+                    .orders()
+                    .withOrderNumber(order.getOrderNumber())
+                    .get()
+                    .executeBlocking()
+                    .getBody();
+
+            Assertions.assertEquals(order.getId(), queriedOrder.getId());
+        });
+    }
+}

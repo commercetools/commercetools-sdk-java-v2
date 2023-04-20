799657e19fa92a06e5528c3135e07c50691c43e5
Jens Schulze <jens.schulze@commercetools.com>
2023-04-20 16:00:43 +0200

    order search builder adjustments

---
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/order/OrderSearchQueryBuilderMixin.java
@@ -0,0 +1,128 @@
+
+package com.commercetools.api.models.order;
+
+import java.util.function.Function;
+
+public interface OrderSearchQueryBuilderMixin {
+    public default OrderSearchQuery and(final OrderSearchQuery... expression) {
+        return OrderSearchAndExpression.builder().and(expression).build();
+    }
+
+    public default OrderSearchQuery or(final OrderSearchQuery... expression) {
+        return OrderSearchOrExpression.builder().or(expression).build();
+    }
+
+    public default OrderSearchQuery not(final OrderSearchQuery... expression) {
+        return OrderSearchNotExpression.builder().not(expression).build();
+    }
+
+    public default OrderSearchQuery filter(final OrderSearchQueryExpression... expression) {
+        return OrderSearchFilterExpression.builder().filter(expression).build();
+    }
+
+    public default OrderSearchQuery exact(final OrderSearchAnyValue value) {
+        return OrderSearchExactExpression.builder().exact(value).build();
+    }
+
+    public default OrderSearchQuery fullText(final OrderSearchFullTextValue value) {
+        return OrderSearchFullTextExpression.builder().fullText(value).build();
+    }
+
+    public default OrderSearchQuery prefix(final OrderSearchStringValue value) {
+        return OrderSearchPrefixExpression.builder().prefix(value).build();
+    }
+
+    public default OrderSearchQuery dateRange(final OrderSearchDateRangeValue value) {
+        return OrderSearchDateRangeExpression.builder().range(value).build();
+    }
+
+    public default OrderSearchQuery numberRange(final OrderSearchNumberRangeValue value) {
+        return OrderSearchNumberRangeExpression.builder().range(value).build();
+    }
+
+    public default OrderSearchQuery longRange(final OrderSearchLongRangeValue value) {
+        return OrderSearchLongRangeExpression.builder().range(value).build();
+    }
+
+    public default OrderSearchQuery wildcard(final OrderSearchStringValue value) {
+        return OrderSearchWildCardExpression.builder().wildcard(value).build();
+    }
+
+    public default OrderSearchQuery exists(final OrderSearchQueryExpressionValue value) {
+        return OrderSearchExistsExpression.builder().exists(value).build();
+    }
+
+    public default OrderSearchQuery and(
+            final Function<OrderSearchAndExpressionBuilder, OrderSearchAndExpressionBuilder> query) {
+        return query.apply(OrderSearchAndExpressionBuilder.of()).build();
+    }
+
+    public default OrderSearchQuery or(
+            final Function<OrderSearchOrExpressionBuilder, OrderSearchOrExpressionBuilder> query) {
+        return query.apply(OrderSearchOrExpressionBuilder.of()).build();
+    }
+
+    public default OrderSearchQuery not(
+            final Function<OrderSearchNotExpressionBuilder, OrderSearchNotExpressionBuilder> query) {
+        return query.apply(OrderSearchNotExpressionBuilder.of()).build();
+    }
+
+    public default OrderSearchQuery filter(
+            final Function<OrderSearchFilterExpressionBuilder, OrderSearchFilterExpressionBuilder> filter) {
+        return filter.apply(OrderSearchFilterExpressionBuilder.of()).build();
+    }
+
+    public default OrderSearchQuery exact(
+            final Function<OrderSearchAnyValueBuilder, OrderSearchAnyValueBuilder> value) {
+        return OrderSearchExactExpression.builder().exact(value.apply(OrderSearchAnyValue.builder()).build()).build();
+    }
+
+    public default OrderSearchQuery fullText(
+            final Function<OrderSearchFullTextValueBuilder, OrderSearchFullTextValueBuilder> value) {
+        return OrderSearchFullTextExpression.builder()
+                .fullText(value.apply(OrderSearchFullTextValue.builder()).build())
+                .build();
+    }
+
+    public default OrderSearchQuery prefix(
+            final Function<OrderSearchStringValueBuilder, OrderSearchStringValueBuilder> value) {
+        return OrderSearchPrefixExpression.builder()
+                .prefix(value.apply(OrderSearchStringValue.builder()).build())
+                .build();
+    }
+
+    public default OrderSearchQuery dateRange(
+            final Function<OrderSearchDateRangeValueBuilder, OrderSearchDateRangeValueBuilder> value) {
+        return OrderSearchDateRangeExpression.builder()
+                .range(value.apply(OrderSearchDateRangeValue.builder()).build())
+                .build();
+    }
+
+    public default OrderSearchQuery numberRange(
+            final Function<OrderSearchNumberRangeValueBuilder, OrderSearchNumberRangeValueBuilder> value) {
+        return OrderSearchNumberRangeExpression.builder()
+                .range(value.apply(OrderSearchNumberRangeValue.builder()).build())
+                .build();
+    }
+
+    public default OrderSearchQuery longRange(
+            final Function<OrderSearchLongRangeValueBuilder, OrderSearchLongRangeValueBuilder> value) {
+        return OrderSearchLongRangeExpression.builder()
+                .range(value.apply(OrderSearchLongRangeValue.builder()).build())
+                .build();
+    }
+
+    public default OrderSearchQuery wildcard(
+            final Function<OrderSearchStringValueBuilder, OrderSearchStringValueBuilder> value) {
+        return OrderSearchWildCardExpression.builder()
+                .wildcard(value.apply(OrderSearchStringValue.builder()).build())
+                .build();
+    }
+
+    public default OrderSearchQuery exists(
+            final Function<OrderSearchQueryExpressionValueBuilder, OrderSearchQueryExpressionValueBuilder> value) {
+        return OrderSearchExistsExpression.builder()
+                .exists(value.apply(OrderSearchQueryExpressionValue.builder()).build())
+                .build();
+    }
+}

3cc9d7cbd542efdd87147ab66c0c1cb1a79b7581
Jens Schulze <jens.schulze@commercetools.com>
2023-05-22 22:01:36 +0200

    add plain enum attribute query builder

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/predicates/query/product/AttributeQueryBuilderMixin.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/predicates/query/product/AttributeQueryBuilderMixin.java
@@ -1,5 +1,20 @@
 
 package com.commercetools.api.predicates.query.product;
 
+import java.util.function.Function;
+
+import com.commercetools.api.predicates.query.CombinationQueryPredicate;
+import com.commercetools.api.predicates.query.ConstantQueryPredicate;
+import com.commercetools.api.predicates.query.ContainerQueryPredicate;
+import com.commercetools.api.predicates.query.product_type.AttributePlainEnumValueQueryBuilderDsl;
+
 public interface AttributeQueryBuilderMixin {
+    default CombinationQueryPredicate<AttributeQueryBuilderDsl> plainEnum(
+            Function<AttributePlainEnumValueQueryBuilderDsl, CombinationQueryPredicate<AttributePlainEnumValueQueryBuilderDsl>> fn) {
+        return new CombinationQueryPredicate<>(ContainerQueryPredicate.of()
+                .parent(new ConstantQueryPredicate("value"))
+                .inner(fn.apply(AttributePlainEnumValueQueryBuilderDsl.of())),
+            AttributeQueryBuilderDsl::of);
+    }
+
 }

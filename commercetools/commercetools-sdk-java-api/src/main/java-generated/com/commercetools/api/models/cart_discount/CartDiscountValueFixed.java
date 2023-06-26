dab0800dd10577b0592314eaa37e6f6bb82b6d12
Jens Schulze <jens.schulze@commercetools.com>
2023-06-26 15:10:08 +0200

    add toDraft helper methods for CartDiscountValue and subtypes

--- commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/cart_discount/CartDiscountValueFixed.java
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/cart_discount/CartDiscountValueFixed.java
@@ -31,7 +31,7 @@ import io.vrap.rmf.base.client.utils.Generated;
  */
 @Generated(value = "io.vrap.rmf.codegen.rendering.CoreCodeGenerator", comments = "https://github.com/commercetools/rmf-codegen")
 @JsonDeserialize(as = CartDiscountValueFixedImpl.class)
-public interface CartDiscountValueFixed extends CartDiscountValue {
+public interface CartDiscountValueFixed extends CartDiscountValue, CartDiscountValueFixedMixin {
 
     /**
      * discriminator value for CartDiscountValueFixed

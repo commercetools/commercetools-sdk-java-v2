7f1a16474e8a0100664d532cd454089641e99016
Jens Schulze <jens.schulze@commercetools.com>
2023-08-11 10:19:41 +0200

    restrict attribute & custom field datetime deserialization to have valid offset information

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/json/AtrributeDeserializer.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/json/AtrributeDeserializer.java
@@ -25,7 +25,7 @@ public class AtrributeDeserializer extends JsonDeserializer<AttributeImpl> {
 
     private static Pattern p = Pattern.compile("^[0-9]");
     private static Pattern dateTime = Pattern
-            .compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}([.][0-9]{1,6})?");
+            .compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}([.][0-9]{1,6})?(Z|[+-][0-9]{2}:[0-9]{2})");
     private static Pattern date = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}");
     private static Pattern time = Pattern.compile("^[0-9]{2}:[0-9]{2}:[0-9]{2}([.][0-9]{1,6})?");
 

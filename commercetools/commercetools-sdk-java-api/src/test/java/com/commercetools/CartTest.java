b265d61d20d2ac8f272eb87b22eace466af1fb44
Jens Schulze <jens.schulze@commercetools.com>
2023-08-15 12:56:35 +0200

    add helper methods to create MoneyBuilder from MonetaryAmount

--- commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/CartTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/CartTest.java
@@ -3,13 +3,17 @@ package com.commercetools;
 
 import static com.commercetools.TestUtils.stringFromResource;
 
+import com.commercetools.api.models.cart.CartAddCustomLineItemAction;
 import com.commercetools.api.models.cart.DirectDiscountReference;
 import com.commercetools.api.models.cart.DiscountedLineItemPrice;
 import com.commercetools.api.models.cart_discount.CartDiscountReference;
+import com.commercetools.api.models.common.CentPrecisionMoneyDraft;
+import com.commercetools.api.models.common.DefaultCurrencyUnits;
 
 import io.vrap.rmf.base.client.utils.json.JsonUtils;
 
 import org.assertj.core.api.Assertions;
+import org.javamoney.moneta.FastMoney;
 import org.junit.jupiter.api.Test;
 
 public class CartTest {
@@ -21,5 +25,26 @@ public class CartTest {
                 .isInstanceOf(DirectDiscountReference.class);
         Assertions.assertThat(price.getIncludedDiscounts().get(1).getDiscount())
                 .isInstanceOf(CartDiscountReference.class);
+    }
+
+    @Test
+    public void cartLineItemMoneyBuilder() {
+        CartAddCustomLineItemAction action = CartAddCustomLineItemAction.builder()
+                .withMoney(
+                    moneyBuilder -> moneyBuilder.centPrecisionDraft(FastMoney.of(1.345, DefaultCurrencyUnits.EUR))
+                            .build())
+                .buildUnchecked();
+
+        Assertions.assertThat(action.getMoney()).isInstanceOf(CentPrecisionMoneyDraft.class);
+        Assertions.assertThat(action.getMoney().getCentAmount()).isEqualTo(134);
+
+        CartAddCustomLineItemAction action2 = CartAddCustomLineItemAction.builder()
+                .withMoney(
+                    moneyBuilder -> moneyBuilder.centPrecisionDraft(FastMoney.of(1.355, DefaultCurrencyUnits.EUR))
+                            .build())
+                .buildUnchecked();
+
+        Assertions.assertThat(action2.getMoney()).isInstanceOf(CentPrecisionMoneyDraft.class);
+        Assertions.assertThat(action2.getMoney().getCentAmount()).isEqualTo(136);
     }
 }

3b6de5c48bba33068fbd432c07b514a30a0997b1
Jens Schulze <jens.schulze@commercetools.com>
2023-11-07 11:47:20 +0100

    add docs about json node attribute/custom field deserialization

--- commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/Serialization.java
+++ commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/Serialization.java
@@ -11,7 +11,7 @@ import io.vrap.rmf.base.client.utils.json.modules.ModuleOptions;
  * The default configured {@link com.fasterxml.jackson.databind.ObjectMapper} uses some modules to correctly work with our API.
  * The details can be found in {@link io.vrap.rmf.base.client.utils.json.JsonUtils#createObjectMapper(ModuleOptions)}</p>
  *
- * <h3>Customization</h3>
+ * <h3 id="customization">Customization</h3>
  *
  * <p>To allow customization of the ObjectMapper the SDK uses {@link java.util.ServiceLoader} for
  * {@link io.vrap.rmf.base.client.utils.json.ModuleSupplier}. Adding a file
@@ -23,13 +23,20 @@ import io.vrap.rmf.base.client.utils.json.modules.ModuleOptions;
  * {@include.file commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/json/ApiModule.java}
  * </p>
  *
- * <h3>Date and time attributes</h3>
+ * <h3 id="date-time-attributes">Date and time attributes</h3>
  *
  * <p>When using Date, Time and DateTime types for product attributes or custom fields the SDK deserializes them as {@link java.time.LocalDate},
  * {@link java.time.LocalTime} and {@link java.time.ZonedDateTime}. As sometimes it may needed to serialize them as {@link String} the ObjectMapper
  * can be configured with {@link ModuleOptions}. The {@link com.commercetools.api.json.ApiModule} also is loading the configuration options
  * using {@link System#getProperty(String)} e.g.: {@link com.commercetools.api.json.ApiModuleOptions#DESERIALIZE_DATE_ATTRIBUTE_AS_STRING commercetools.deserializeDateAttributeAsString}</p>
  * {@include.example example.SerializationTest#dateAsString()}</p>
+ *
+ * <h3 id="JsonNodes">Use JsonNodes</h3>
+ *
+ * <p>In case the automatic deserialization of attributes is not needed. You can set the {@link com.commercetools.api.json.ApiModuleOptions#DESERIALIZE_ATTRIBUTE_AS_JSON_NODE commercetools.deserializeAttributeAsJsonNode} an/or
+ * {@link com.commercetools.api.json.ApiModuleOptions#DESERIALIZE_CUSTOM_FIELD_AS_JSON_NODE commercetools.deserializeCustomFieldAsJsonNode}</p>
+ *
+ * {@include.example example.SerializationTest#jsonNodes()}
  */
 public class Serialization {
 }

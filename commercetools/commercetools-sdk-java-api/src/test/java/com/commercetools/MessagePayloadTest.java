461edd5d35804b97d0fc8400dbde28c6279466ec
Jens Schulze <jens.schulze@commercetools.com>
2023-11-08 10:26:57 +0100

    deserialize message payload

--- commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/MessagePayloadTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/MessagePayloadTest.java
@@ -1,10 +1,15 @@
 
 package com.commercetools;
 
+import static com.commercetools.TestUtils.stringFromResource;
+
 import java.io.IOException;
 
+import com.commercetools.api.models.message.CustomerFirstNameSetMessagePayload;
+import com.commercetools.api.models.message.CustomerLastNameSetMessagePayload;
 import com.commercetools.api.models.message.MessagePayload;
 import com.commercetools.api.models.message.OrderCreatedMessagePayload;
+import com.commercetools.api.models.subscription.MessageDeliveryPayload;
 
 import io.vrap.rmf.base.client.utils.json.JsonUtils;
 
@@ -19,5 +24,17 @@ public class MessagePayloadTest {
         MessagePayload deserializedObject = JsonUtils.fromJsonString(payload, MessagePayload.class);
 
         Assertions.assertThat(deserializedObject).isInstanceOf(OrderCreatedMessagePayload.class);
+    }
+
+    @Test
+    public void deserializeMessageDeliveryPayload() {
+        MessageDeliveryPayload delivery = JsonUtils.fromJsonString(stringFromResource("messagedeliverypayload.json"),
+            MessageDeliveryPayload.class);
+
+        Assertions.assertThat(delivery.getMessage()).isInstanceOf(CustomerLastNameSetMessagePayload.class);
+        Assertions.assertThat(delivery.getMessage().as(CustomerLastNameSetMessagePayload.class))
+                .isInstanceOf(CustomerLastNameSetMessagePayload.class);
+        Assertions.assertThatThrownBy(() -> delivery.getMessage().as(CustomerFirstNameSetMessagePayload.class))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

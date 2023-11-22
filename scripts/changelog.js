76368a44291d9b62930e1e147fccd548a07f76ff
Jens Schulze <jens.schulze@commercetools.com>
2023-11-22 10:26:35 +0100

    add changelog script

---
+++ scripts/changelog.js
@@ -0,0 +1,17 @@
+
+const releases = require("../releases.json")
+
+var changeLog = ""
+releases.filter((release) => !release.tag_name.startsWith("0") && release.draft == false && release.prerelease == false ).forEach(release => {
+
+  changeLog += `
+# ${release.tag_name} (${release.published_at})
+
+${release.body}
+`
+
+//console.log(release)
+})
+
+console.log(changeLog)
+

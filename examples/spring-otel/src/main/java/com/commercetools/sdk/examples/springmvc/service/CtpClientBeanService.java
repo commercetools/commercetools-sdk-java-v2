f6482f12a36f7f088a69847982420638693a2bc1
lojzatran <lam.tran@commercetools.com>
2023-11-30 21:45:42 +0100

    Revert wrong file

--- examples/spring-otel/src/main/java/com/commercetools/sdk/examples/springmvc/service/CtpClientBeanService.java
+++ examples/spring-otel/src/main/java/com/commercetools/sdk/examples/springmvc/service/CtpClientBeanService.java
@@ -1,9 +1,11 @@
 
 package com.commercetools.sdk.examples.springmvc.service;
 
+import com.commercetools.api.client.ProjectApiRoot;
 import com.commercetools.api.client.ProjectScopedApiRoot;
 import com.commercetools.api.defaultconfig.ApiRootBuilder;
 
+import com.commercetools.monitoring.opentelemetry.OpenTelemetryMiddleware;
 import com.commercetools.monitoring.opentelemetry.OpenTelemetryResponseSerializer;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.vrap.rmf.base.client.*;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 @EnableAutoConfiguration
@@ -27,27 +30,16 @@ public class CtpClientBeanService {
     @Value(value = "${ctp.project.key}")
     private String projectKey;
 
-    @Value(value = "${ctp.enable.histogram:true}")
-    private boolean enableHistogram;
-
     private ClientCredentials credentials() {
         return ClientCredentials.of().withClientId(clientId).withClientSecret(clientSecret).build();
     }
 
     @Bean
     public ProjectScopedApiRoot apiRoot() {
-        if (enableHistogram){
-            return ApiRootBuilder.of()
-                    .defaultClient(credentials())
-                    .withSerializer(new OpenTelemetryResponseSerializer(ResponseSerializer.of(), GlobalOpenTelemetry.get()))
-                    .withTelemetryMiddleware(new OpenTelemetryMiddleware(GlobalOpenTelemetry.get()))
-                    .build(projectKey);
-        } else {
-            return ApiRootBuilder.of()
-                    .defaultClient(credentials())
-                    .withTelemetryMiddleware(new OpenTelemetryMiddleware(GlobalOpenTelemetry.get(), false))
-                    .build(projectKey);
-        }
-
+        return ApiRootBuilder.of()
+                .defaultClient(credentials())
+                .withSerializer(new OpenTelemetryResponseSerializer(ResponseSerializer.of(), GlobalOpenTelemetry.get()))
+                .withTelemetryMiddleware(new OpenTelemetryMiddleware(GlobalOpenTelemetry.get()))
+                .build(projectKey);
     }
 }

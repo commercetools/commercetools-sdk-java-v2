71adee21cf5905e9a319e9e5db5ef6ed96c703e4
Auto Mation <automation@commercetools.com>
2023-12-07 14:21:42 +0000

    build(codegen): updating SDK

--- commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/product_type/AttributeReferenceType.java
+++ commercetools/commercetools-sdk-java-api/src/main/java-generated/com/commercetools/api/models/product_type/AttributeReferenceType.java
@@ -21,7 +21,7 @@ import io.vrap.rmf.base.client.utils.Generated;
  * <div class=code-example>
  * <pre><code class='java'>
  *     AttributeReferenceType attributeReferenceType = AttributeReferenceType.builder()
- *             .referenceTypeId(AttributeReferenceTypeId.CART)
+ *             .referenceTypeId(AttributeReferenceTypeId.ASSOCIATE_ROLE)
  *             .build()
  * </code></pre>
  * </div>

cb45d5c8a32313cb8bedbe23a64be6be2fe72691
lojzatran <lam.tran@commercetools.com>
2024-03-27 22:44:56 +0100

    Add test to get access token from apiRoot

--- commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/oauth/GlobalCustomerPasswordAuthIntegrationTest.java
+++ commercetools/commercetools-sdk-java-api/src/integrationTest/java/commercetools/oauth/GlobalCustomerPasswordAuthIntegrationTest.java
@@ -3,19 +3,28 @@ package commercetools.oauth;
 
 import static commercetools.utils.CommercetoolsTestUtils.*;
 
+import java.security.MessageDigest;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
+import com.commercetools.api.client.ApiRoot;
+import com.commercetools.api.defaultconfig.ApiRootBuilder;
 import com.commercetools.api.defaultconfig.ServiceRegion;
 import com.commercetools.api.models.customer.Customer;
 import com.commercetools.api.models.customer.CustomerDraft;
 import com.commercetools.api.models.customer.CustomerDraftBuilder;
+import com.commercetools.api.models.project.Project;
 import commercetools.customer.CustomerFixtures;
 import commercetools.utils.CommercetoolsTestUtils;
 
+import io.vrap.rmf.base.client.ApiHttpResponse;
 import io.vrap.rmf.base.client.AuthenticationToken;
 import io.vrap.rmf.base.client.HttpClientSupplier;
 import io.vrap.rmf.base.client.VrapHttpClient;
+import io.vrap.rmf.base.client.http.HttpStatusCode;
+import io.vrap.rmf.base.client.http.OAuthHandler;
+import io.vrap.rmf.base.client.http.OAuthMiddleware;
 import io.vrap.rmf.base.client.oauth2.GlobalCustomerPasswordTokenSupplier;
 import io.vrap.rmf.base.client.oauth2.InMemoryTokenStorage;
 import io.vrap.rmf.base.client.oauth2.RefreshFlowTokenSupplier;
@@ -83,9 +92,49 @@ public class GlobalCustomerPasswordAuthIntegrationTest {
             AuthenticationToken newAuthenticationToken = ClientUtils
                     .blockingWait(refreshFlowTokenSupplier.refreshToken(), Duration.ofSeconds(10));
 
-            Assertions.assertNotEquals(authenticationToken.getAccessToken(), newAuthenticationToken.getAccessToken());
+            Assertions.assertNotEquals(calculateSha256(authenticationToken.getAccessToken()), calculateSha256(newAuthenticationToken.getAccessToken()));
             Assertions.assertTrue(authenticationToken.getExpiresInZonedDateTime()
                     .isBefore(newAuthenticationToken.getExpiresInZonedDateTime()));
+        });
+    }
+
+    @Test
+    public void getNewAccessTokenFromApiRoot() {
+        CustomerFixtures.withCustomer(customer -> {
+            GlobalCustomerPasswordTokenSupplier globalCustomerPasswordTokenSupplier = new GlobalCustomerPasswordTokenSupplier(
+                    getClientId(), getClientSecret(), customer.getEmail(), CustomerFixtures.TEST_CUSTOMER_PASSWORD, null,
+                    ServiceRegion.GCP_EUROPE_WEST1.getPasswordFlowTokenURL(CommercetoolsTestUtils.getProjectKey()),
+                    vrapHttpClient);
+
+            AuthenticationToken authenticationToken = ClientUtils
+                    .blockingWait(globalCustomerPasswordTokenSupplier.getToken(), Duration.ofSeconds(10));
+            String oldAccessToken = authenticationToken.getAccessToken();
+            authenticationToken.setExpiresIn(0L);
+
+            InMemoryTokenStorage tokenStorage = new InMemoryTokenStorage(authenticationToken);
+
+            RefreshFlowTokenSupplier refreshFlowTokenSupplier = new RefreshFlowTokenSupplier(getClientId(),
+                    getClientSecret(), ServiceRegion.GCP_EUROPE_WEST1.getOAuthTokenUrl(), tokenStorage, vrapHttpClient);
+
+            OAuthHandler oAuthHandler = new OAuthHandler(refreshFlowTokenSupplier, Duration.ofSeconds(60));
+
+            ApiRoot apiRoot = ApiRootBuilder.ofEnvironmentVariables()
+                    .withOAuthMiddleware(OAuthMiddleware.of(oAuthHandler))
+                    .withPolicies(policyBuilder -> policyBuilder.withRetry(builder -> builder.maxRetries(5)
+                            .statusCodes(Arrays.asList(HttpStatusCode.BAD_GATEWAY_502,
+                                    HttpStatusCode.SERVICE_UNAVAILABLE_503, HttpStatusCode.GATEWAY_TIMEOUT_504))
+                    ))
+                    .build();
+
+
+            apiRoot.withProjectKey(getProjectKey())
+                    .get()
+                    .execute()
+                    .thenApply(ApiHttpResponse::getBody)
+                    .thenApply(Project::getName)
+                    .join();
+
+            Assertions.assertNotEquals(calculateSha256(oldAccessToken), calculateSha256(tokenStorage.getToken().getAccessToken()));
         });
     }
 
@@ -100,4 +149,22 @@ public class GlobalCustomerPasswordAuthIntegrationTest {
             globalCustomerPasswordTokenSupplier.getToken().get();
         });
     }
+
+    public static String calculateSha256(final String base) {
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
 }

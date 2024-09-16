68aa851f57a53b889d6705ac11d76276776883d2
Jens Schulze <jens.schulze@commercetools.com>
2024-09-16 12:59:07 +0200

    add response serializer test

--- commercetools/commercetools-monitoring-datadog/src/main/java/com/commercetools/monitoring/datadog/DatadogResponseSerializer.java
+++ commercetools/commercetools-monitoring-datadog/src/main/java/com/commercetools/monitoring/datadog/DatadogResponseSerializer.java
@@ -25,6 +25,11 @@ public class DatadogResponseSerializer implements ResponseSerializer {
 
     private final MetricsApi apiInstance;
 
+    public DatadogResponseSerializer(final ResponseSerializer serializer, final MetricsApi apiInstance) {
+        this.serializer = serializer;
+        this.apiInstance = apiInstance;
+    }
+
     public DatadogResponseSerializer(final ResponseSerializer serializer, final ApiClient ddApiClient) {
         this.serializer = serializer;
         this.apiInstance = new MetricsApi(ddApiClient);

7edd50abfd3c4878db0e2a9fb90d4e02c482edc5
lojzatran <lam.tran@commercetools.com>
2024-11-20 10:43:30 +0100

    SUPPORT-28804 add class for backwards compatibility

---
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/json/AtrributeDeserializer.java
@@ -0,0 +1,19 @@
+package com.commercetools.api.json;
+
+/**
+ * @deprecated typo in the class name, use AttributeDeserializer instead
+ */
+@Deprecated(forRemoval = true)
+public class AtrributeDeserializer extends AttributeDeserializer {
+    public AtrributeDeserializer(boolean deserializeAsDateString) {
+        super(deserializeAsDateString);
+    }
+
+    public AtrributeDeserializer(boolean deserializeAsDateString, boolean deserializeNumberAsDouble) {
+        super(deserializeAsDateString, deserializeNumberAsDouble);
+    }
+
+    public AtrributeDeserializer() {
+        super();
+    }
+}

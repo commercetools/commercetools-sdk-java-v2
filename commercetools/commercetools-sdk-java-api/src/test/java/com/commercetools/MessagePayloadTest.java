7c684d8e44ad72004fe20bc743484429cca39142
Jens Schulze <jens.schulze@commercetools.com>
2025-04-03 00:21:17 +0200

    fix Delivery discriminators

--- commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/MessagePayloadTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/MessagePayloadTest.java
@@ -9,8 +9,7 @@ import com.commercetools.api.models.message.CustomerFirstNameSetMessagePayload;
 import com.commercetools.api.models.message.CustomerLastNameSetMessagePayload;
 import com.commercetools.api.models.message.MessagePayload;
 import com.commercetools.api.models.message.OrderCreatedMessagePayload;
-import com.commercetools.api.models.subscription.DeliveryPayload;
-import com.commercetools.api.models.subscription.MessageDeliveryPayload;
+import com.commercetools.api.models.subscription.*;
 
 import io.vrap.rmf.base.client.utils.json.JsonUtils;
 
@@ -18,6 +17,45 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MessagePayloadTest {
+
+    @Test
+    public void delivery() {
+        String messagePayload = "{\n" + "  \"notificationType\":\"Message\",\n"
+                + "  \"projectKey\":\"<project_key>\",\n" + "  \"id\":\"<message_id>\",\n" + "  \"version\":1,\n"
+                + "  \"sequenceNumber\":2,\n" + "  \"resource\":{\n" + "     \"typeId\":\"customer\",\n"
+                + "     \"id\":\"<customer_id>\"\n" + "  },\n" + "  \"resourceVersion\":2,\n"
+                + "  \"resourceUserProvidedIdentifiers\":{},\n" + "  \"type\":\"CustomerLastNameSet\",\n"
+                + "  \"lastName\":\"Doe\",\n" + "  \"createdAt\":\"2022-10-25T13:30:09.760Z\",\n"
+                + "  \"lastModifiedAt\":\"2022-10-25T13:30:09.760Z\"\n" + "}";
+        SubscriptionNotification deserializedObject = JsonUtils.fromJsonString(messagePayload,
+            SubscriptionNotification.class);
+        Assertions.assertThat(deserializedObject).isInstanceOf(MessageDeliveryPayload.class);
+
+        deserializedObject = JsonUtils.fromJsonString(messagePayload, DeliveryPayload.class);
+        Assertions.assertThat(deserializedObject).isInstanceOf(MessageDeliveryPayload.class);
+
+        String eventPayload = "{\n" + "  \"id\": \"3b364ca2-cbe5-42b4-a85a-37d4e57d659e\",\n"
+                + "  \"notificationType\": \"Event\",\n" + "  \"type\": \"ImportContainerCreated\",\n"
+                + "  \"resourceType\": \"import-api\",\n" + "  \"data\": {\n" + "    \"version\": 1,\n"
+                + "    \"key\": \"my-import-container\",\n" + "    \"createdAt\": \"2025-03-26T17:28:20.376Z\",\n"
+                + "    \"lastModifiedAt\": \"2025-03-26T17:28:20.376Z\"\n" + "  },\n"
+                + "  \"createdAt\": \"2025-03-26T17:28:20.397Z\"\n" + "}\n";
+        deserializedObject = JsonUtils.fromJsonString(eventPayload, SubscriptionNotification.class);
+        Assertions.assertThat(deserializedObject).isInstanceOf(EventDeliveryPayload.class);
+
+        String changePayload = "{\n" + "  \"notificationType\":\"ResourceCreated\",\n"
+                + "  \"projectKey\":\"<project_key>\",\n" + "  \"resource\":{\n" + "     \"typeId\":\"product\",\n"
+                + "     \"id\":\"<product_id>\"\n" + "  },\n" + "  \"resourceUserProvidedIdentifiers\":{\n"
+                + "     \"key\":\"example-product-key\",\n" + "     \"slug\":{\n" + "        \"en\":\"example-slug\"\n"
+                + "     }\n" + "  },\n" + "  \"version\":1,\n" + "  \"modifiedAt\":\"2022-10-25T13:23:05.384Z\"\n"
+                + "}\n";
+        deserializedObject = JsonUtils.fromJsonString(changePayload, SubscriptionNotification.class);
+        Assertions.assertThat(deserializedObject).isInstanceOf(ResourceCreatedDeliveryPayload.class);
+
+        deserializedObject = JsonUtils.fromJsonString(changePayload, DeliveryPayload.class);
+        Assertions.assertThat(deserializedObject).isInstanceOf(ResourceCreatedDeliveryPayload.class);
+
+    }
 
     @Test
     public void deserializeOrderCreatedMessageFromString() throws IOException {

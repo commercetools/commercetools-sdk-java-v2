c5d814bb3142c659bff215c208df81cd7c550fdd
Jens Schulze <jens.schulze@commercetools.com>
2025-05-22 14:26:00 +0200

    add docs for split packages

---
+++ commercetools/internal-docs/src/main/java/com/commercetools/docs/meta/SizeOptimization.java
@@ -0,0 +1,26 @@
+
+package com.commercetools.docs.meta;
+
+/**
+ * {@include.toc}
+ * <h2>Optimizing package size</h2>
+ *
+ * <p>Optimizing the size of applications in a size constrained environment like AWS Lambda can
+ * be challenging.</p>
+ *
+ * <h3>Using split packages</h3>
+ *
+ * <p>For an application which covers the whole business logic and may act as a backend
+ * for frontend it's advised to use the <code>commercetools-sdk-api</code> package.</p>
+ *
+ * <p>When developing small function applications which will be used in a size constrained environment
+ * the SDK offers packages split by their domain. Due to the connections between the different domains
+ * the split is realized after compilation and doesn't reflect the whole dependency graph of all packages.
+ * You will have to explicitly define the packages you intend to use.</p>
+ *
+ * <p>We provide an {@see <a href=https://github.com/commercetools/commercetools-sdk-java-v2/tree/main/examples/split-packages>example</a>} for using split packages</p>
+ *
+ * <p>The packages always start with the prefix <code>commercetools-sdk-java-api-models</code></p>
+ */
+public class SizeOptimization {
+}

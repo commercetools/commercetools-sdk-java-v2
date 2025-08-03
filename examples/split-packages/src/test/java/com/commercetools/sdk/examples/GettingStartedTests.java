89f42172c30f36057a4a2a7cd90d2d67a95478be
Jens Schulze <jens.schulze@commercetools.com>
2025-08-03 12:26:42 +0200

    add test for category query

--- examples/split-packages/src/test/java/com/commercetools/sdk/examples/GettingStartedTests.java
+++ examples/split-packages/src/test/java/com/commercetools/sdk/examples/GettingStartedTests.java
@@ -3,6 +3,7 @@ package com.commercetools.sdk.examples;
 import com.commercetools.api.client.ProjectApiRoot;
 import com.commercetools.api.defaultconfig.ApiRootBuilder;
 import com.commercetools.api.defaultconfig.ServiceRegion;
+import com.commercetools.api.models.category.CategoryPagedQueryResponse;
 import com.commercetools.api.models.project.Project;
 import io.vrap.rmf.base.client.oauth2.ClientCredentials;
 import org.junit.jupiter.api.Assertions;
@@ -25,5 +26,22 @@ public class GettingStartedTests {
                 .executeBlocking().getBody();
 
         Assertions.assertEquals(System.getenv("CTP_PROJECT_KEY"), response.getKey());
+    }
+
+    @Test
+    public void category() {
+        ProjectApiRoot apiRoot = ApiRootBuilder.of().defaultClient(
+                ClientCredentials.of().withClientId(System.getenv("CTP_CLIENT_ID"))
+                        .withClientSecret(System.getenv("CTP_CLIENT_SECRET"))
+                        .build(),
+                ServiceRegion.valueOf(Optional.ofNullable(System.getenv("CTP_SERVICE_REGION")).orElse("GCP_EUROPE_WEST1"))).build(System.getenv("CTP_PROJECT_KEY"));
+
+        CategoryPagedQueryResponse response = apiRoot
+                .categories()
+                .get()
+                .addLimit(500)
+                .executeBlocking().getBody();
+
+        Assertions.assertNotNull(response);
     }
 }

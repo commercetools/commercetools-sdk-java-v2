2899aacad3d8dc65589a3216804bcc94e9ca3cbe
Anna Shipil <anna.shipil@commercetools.com>
2025-08-21 14:53:32 +0200

    Fix product Mixin Product level attributes

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/product/ProductDataLike.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/product/ProductDataLike.java
@@ -109,4 +109,6 @@ public interface ProductDataLike extends WithLocalizedSlug, MetaAttributes {
 
     @Nullable
     CategoryOrderHints getCategoryOrderHints();
+
+    List<Attribute> getAttributes();
 }

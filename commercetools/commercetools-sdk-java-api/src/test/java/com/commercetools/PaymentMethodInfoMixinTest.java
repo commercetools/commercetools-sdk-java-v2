e9c5dcbdb603205ba9cf4ac778dbf766e5ccf4eb
Anna Shipil <anna.shipil@commercetools.com>
2025-09-22 09:57:25 +0200

    SUPPORT-35419: fixing assertion

--- commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/PaymentMethodInfoMixinTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/PaymentMethodInfoMixinTest.java
@@ -7,12 +7,13 @@ import com.commercetools.api.models.common.Money;
 import com.commercetools.api.models.customer.CustomerResourceIdentifierBuilder;
 import com.commercetools.api.models.payment.PaymentDraft;
 import com.commercetools.api.models.payment.PaymentMethodInfo;
+import com.commercetools.api.models.payment.PaymentMethodInfoDraft;
 import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentMethodInfoMixinTest {
     @Test
@@ -23,6 +24,6 @@ public class PaymentMethodInfoMixinTest {
                 .name(LocalizedString.of())
                 .build();
 
-        assertNotNull(paymentMethodInfo.toDraft());
+        assertInstanceOf(PaymentMethodInfoDraft.class, paymentMethodInfo.toDraft());
     }
 }

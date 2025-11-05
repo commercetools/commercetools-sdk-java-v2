b73edf61b3d9dc99224646b49a5cfb8d7e85e3db
Anna Shipil <anna.shipil@commercetools.com>
2025-11-05 11:00:43 +0100

    DEVX-657: adding a test for the support issue

--- commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/CategoriesTest.java
+++ commercetools/commercetools-sdk-java-api/src/test/java/com/commercetools/CategoriesTest.java
@@ -2,12 +2,19 @@
 package com.commercetools;
 
 import java.net.URI;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Locale;
 
 import com.commercetools.api.client.ByProjectKeyCategoriesPost;
-import com.commercetools.api.models.common.LocalizedStringEntry;
 
+import com.commercetools.api.models.category.Category;
+import com.commercetools.api.models.category.CategoryTreeFactory;
+import com.commercetools.api.models.common.LocalizedString;
+import com.commercetools.api.models.common.LocalizedStringEntry;
 import io.vrap.rmf.base.client.ApiHttpRequest;
 import io.vrap.rmf.base.client.SerializerOnlyApiHttpClient;
+import com.commercetools.api.models.common.LocalizedStringImpl;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -39,5 +46,16 @@ public class CategoriesTest {
         var entry = LocalizedStringEntry.of("en", "women");
         var entry2 = LocalizedStringEntry.of("en", "women");
         Assertions.assertThat(entry).isEqualTo(entry2);
+    }
+
+    @Test
+    public void testCategoryTree() {
+        List<Category> allCategoriesAsFlatList = List.of(Category.builder().name(LocalizedString.of(Locale.ENGLISH, "Women"))
+                .slug(LocalizedString.of(Locale.ENGLISH, "women")).id("1").version(1L).createdAt(ZonedDateTime.now()).lastModifiedAt(ZonedDateTime.now()).ancestors()
+                .orderHint("c2").build());
+        CategoryTreeFactory factory = CategoryTreeFactory.of();
+        var tree = factory.create(allCategoriesAsFlatList);
+        var result = tree.findBySlug(Locale.ENGLISH, "women");
+        Assertions.assertThat(result).isNotEmpty();
     }
 }

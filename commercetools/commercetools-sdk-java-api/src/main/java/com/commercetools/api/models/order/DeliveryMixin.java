41ec4b0af74428080c57a82047ce62294bd346fe
Anna Shipil <anna.shipil@commercetools.com>
2026-02-10 17:52:06 +0100

    DEVX-741: managing NullPointerException

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/order/DeliveryMixin.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/models/order/DeliveryMixin.java
@@ -2,6 +2,7 @@
 package com.commercetools.api.models.order;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.commercetools.api.models.common.Address;
 import com.commercetools.api.models.type.CustomFields;
@@ -17,10 +18,10 @@ public interface DeliveryMixin {
 
     public default DeliveryDraftBuilder toDraftBuilder() {
         return DeliveryDraft.builder()
-                .address(this.getAddress().toDraft())
+                .address(Optional.ofNullable(this.getAddress()).map(Address::toDraft).orElse(null))
                 .items(this.getItems())
                 .parcels(this.getParcels().stream().map(Parcel::toDraft).collect(java.util.stream.Collectors.toList()))
-                .custom(this.getCustom().toDraft());
+                .custom(Optional.ofNullable(this.getCustom()).map(CustomFields::toDraft).orElse(null));
     }
 
     public default DeliveryDraft toDraft() {

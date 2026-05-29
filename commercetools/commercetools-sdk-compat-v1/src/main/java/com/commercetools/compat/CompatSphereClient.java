1ca395c3504241ee0423333254d7d76e95c233b2
Anna Shipil <anna.shipil@commercetools.com>
2026-05-29 12:38:38 +0200

    DEVX-796: fixing SphereJsonUtils due to jackson 2->3 update

--- commercetools/commercetools-sdk-compat-v1/src/main/java/com/commercetools/compat/CompatSphereClient.java
+++ commercetools/commercetools-sdk-compat-v1/src/main/java/com/commercetools/compat/CompatSphereClient.java
@@ -11,13 +11,13 @@ import com.commercetools.api.client.ProjectApiRoot;
 import com.commercetools.api.defaultconfig.ApiRootBuilder;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
+import io.vrap.rmf.base.client.utils.json.JsonUtils;
 
 import io.sphere.sdk.client.*;
 import io.sphere.sdk.http.HttpHeaders;
 import io.sphere.sdk.http.HttpRequest;
 import io.sphere.sdk.http.HttpResponse;
 import io.sphere.sdk.json.JsonException;
-import io.sphere.sdk.json.SphereJsonUtils;
 import io.sphere.sdk.models.SphereException;
 import io.vrap.rmf.base.client.*;
 import io.vrap.rmf.base.client.oauth2.ClientCredentials;
@@ -65,7 +65,7 @@ public class CompatSphereClient extends AutoCloseableService implements SphereCl
      * @return SphereClient using a SDK v2 ApiHttpClient as HTTP client
      */
     public static CompatSphereClient of(SphereClientConfig clientConfig) {
-        return of(createDefaultClient(clientConfig), SphereJsonUtils.newObjectMapper(), clientConfig.getProjectKey(),
+        return of(createDefaultClient(clientConfig), JsonUtils.createObjectMapper(), clientConfig.getProjectKey(),
             ExceptionMode.SDK_V2);
     }
 
@@ -75,7 +75,7 @@ public class CompatSphereClient extends AutoCloseableService implements SphereCl
      * @return SphereClient using a SDK v2 ApiHttpClient as HTTP client
      */
     public static CompatSphereClient of(final ProjectApiRoot apiRoot) {
-        return of(apiRoot.getApiHttpClient(), SphereJsonUtils.newObjectMapper(), apiRoot.getProjectKey(),
+        return of(apiRoot.getApiHttpClient(), JsonUtils.createObjectMapper(), apiRoot.getProjectKey(),
             ExceptionMode.SDK_V1);
     }
 
@@ -96,7 +96,7 @@ public class CompatSphereClient extends AutoCloseableService implements SphereCl
      * @return SphereClient using a SDK v2 ApiHttpClient as HTTP client
      */
     public static CompatSphereClient of(final ApiHttpClient apiHttpClient, final String projectKey) {
-        return of(apiHttpClient, SphereJsonUtils.newObjectMapper(), projectKey, ExceptionMode.SDK_V1);
+        return of(apiHttpClient, JsonUtils.createObjectMapper(), projectKey, ExceptionMode.SDK_V1);
     }
 
     /**
@@ -118,7 +118,7 @@ public class CompatSphereClient extends AutoCloseableService implements SphereCl
      * @return SphereClient using a SDK v2 ApiHttpClient as HTTP client
      */
     public static CompatSphereClient of(SphereClientConfig clientConfig, final ExceptionMode exceptionMode) {
-        return of(createDefaultClient(clientConfig), SphereJsonUtils.newObjectMapper(), clientConfig.getProjectKey(),
+        return of(createDefaultClient(clientConfig), JsonUtils.createObjectMapper(), clientConfig.getProjectKey(),
             exceptionMode);
     }
 
@@ -129,7 +129,7 @@ public class CompatSphereClient extends AutoCloseableService implements SphereCl
      * @return SphereClient using a SDK v2 ApiHttpClient as HTTP client
      */
     public static CompatSphereClient of(final ProjectApiRoot apiRoot, final ExceptionMode exceptionMode) {
-        return of(apiRoot.getApiHttpClient(), SphereJsonUtils.newObjectMapper(), apiRoot.getProjectKey(),
+        return of(apiRoot.getApiHttpClient(), JsonUtils.createObjectMapper(), apiRoot.getProjectKey(),
             exceptionMode);
     }
 
@@ -154,7 +154,7 @@ public class CompatSphereClient extends AutoCloseableService implements SphereCl
      */
     public static CompatSphereClient of(final ApiHttpClient apiHttpClient, final String projectKey,
             final ExceptionMode exceptionMode) {
-        return of(apiHttpClient, SphereJsonUtils.newObjectMapper(), projectKey, exceptionMode);
+        return of(apiHttpClient, JsonUtils.createObjectMapper(), projectKey, exceptionMode);
     }
 
     /**

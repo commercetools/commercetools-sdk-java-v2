e7350d6aa6e699680bdbcc015e83c3e2433bf5ec
Jens Schulze <jens.schulze@commercetools.com>
2026-09-09 10:49:35 +0200

    add agents to projectapiroot

--- commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/client/ProjectApiRoot.java
+++ commercetools/commercetools-sdk-java-api/src/main/java/com/commercetools/api/client/ProjectApiRoot.java
@@ -334,6 +334,11 @@ public class ProjectApiRoot implements Closeable, ProjectScopedApiRoot {
     }
 
     @Override
+    public ByProjectKeyAgentsRequestBuilder agents() {
+        return with().agents();
+    }
+
+    @Override
     public <R> R with(Function<ProjectApiRoot, R> op) {
         return op.apply(this);
     }
